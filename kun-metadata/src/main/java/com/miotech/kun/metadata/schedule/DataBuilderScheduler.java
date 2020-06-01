18862112430324e6a5b463f964ecab883ddae715
zhangxinwei <xinwei.zhang@mioying.com>
2020-06-01 14:30:15 +0800

    Feature:SP-3965 optimize startup parameters

--- kun-metadata/src/main/java/com/miotech/kun/metadata/schedule/DataBuilderScheduler.java
+++ kun-metadata/src/main/java/com/miotech/kun/metadata/schedule/DataBuilderScheduler.java
@@ -14,19 +14,26 @@ public class DataBuilderScheduler {
     private static Logger logger = LoggerFactory.getLogger(DataBuilderScheduler.class);
     private static final long period = 86400L;
     private static long initialDelay = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).toEpochSecond(ZoneOffset.of("+8")) - LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
+    private static String env = "dev";
 
     public static void main(String[] args) {
-        try {
-            if (args != null && args.length == 1) {
-                initialDelay = Long.parseLong(args[0]);
+        int length = args.length;
+        if (length == 1) {
+            env = String.format("application-%s.conf", args[0]);
+        } else if (length == 2) {
+            env = String.format("application-%s.conf", args[0]);
+            try {
+                initialDelay = Long.parseLong(args[1]);
+            } catch (NumberFormatException numberFormatException) {
+                logger.error("[initialDelay] should be number");
+                throw ExceptionUtils.wrapIfChecked(numberFormatException);
             }
-        } catch (NumberFormatException numberFormatException) {
-            logger.error("initialDelay should be number");
-            throw ExceptionUtils.wrapIfChecked(numberFormatException);
+        } else {
+            throw new IllegalArgumentException("Param error, Usage [env] [initialDelay]");
         }
 
         // load datasource
-        Injector injector = Guice.createInjector(new DataSourceModule());
+        Injector injector = Guice.createInjector(new DataSourceModule(env));
         DataBuilder dataBuilder = injector.getInstance(DataBuilder.class);
         dataBuilder.scheduleAtRate(initialDelay, period, TimeUnit.SECONDS);
     }
