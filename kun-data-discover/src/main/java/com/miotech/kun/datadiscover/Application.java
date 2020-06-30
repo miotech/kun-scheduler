17724c8c8d0c3b16f8c5d20e59c28111c1576b3b
bonnie1119 <bonnie.chen@mioying.com>
2020-06-30 15:16:39 +0800

    ER: SP-4166 test

--- kun-data-discover/src/main/java/com/miotech/kun/datadiscover/Application.java
+++ kun-data-discover/src/main/java/com/miotech/kun/datadiscover/Application.java
@@ -3,6 +3,8 @@ package com.miotech.kun.datadiscover;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 /**
  * @author: Melo
  * @created: 5/26/20
