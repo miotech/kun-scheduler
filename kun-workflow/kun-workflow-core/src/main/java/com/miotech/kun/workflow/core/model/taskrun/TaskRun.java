7fc1706fdcdb0936bfaa50bd51598453e9a57672
jentle <jiang.gu@mioying.com>
2020-07-14 15:45:59 +0800

    FIX: SP-4312

--- kun-workflow/kun-workflow-core/src/main/java/com/miotech/kun/workflow/core/model/taskrun/TaskRun.java
+++ kun-workflow/kun-workflow-core/src/main/java/com/miotech/kun/workflow/core/model/taskrun/TaskRun.java
@@ -1,7 +1,5 @@
 package com.miotech.kun.workflow.core.model.taskrun;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.miotech.kun.workflow.core.model.common.Tick;
 import com.miotech.kun.workflow.core.model.common.Variable;
 import com.miotech.kun.workflow.core.model.lineage.DataStore;
@@ -13,7 +11,6 @@ import java.util.List;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class TaskRun {
-    @JsonSerialize(using = ToStringSerializer.class)
     private final Long id;
 
     private final Task task;
@@ -32,7 +29,6 @@ public class TaskRun {
 
     private final List<DataStore> outlets;
 
-    @JsonSerialize(contentUsing = ToStringSerializer.class)
     private final List<Long> dependentTaskRunIds;
 
     public Long getId() {
