e33986c16bc47356feeb2ce83ad4ebae481507ee
zhangxinwei <xinwei.zhang@mioying.com>
2020-07-16 19:33:11 +0800

    Feature:SP-4364 create build all operator&task

--- kun-metadata/kun-metadata-web/src/main/java/com/miotech/kun/metadata/web/constant/WorkflowApiParam.java
+++ kun-metadata/kun-metadata-web/src/main/java/com/miotech/kun/metadata/web/constant/WorkflowApiParam.java
@@ -5,12 +5,15 @@ public class WorkflowApiParam {
     private WorkflowApiParam() {
     }
 
-    public static final String OPERATOR_NAME = "Metadata DataBuilder Operator";
-    public static final String TASK_NAME = "Metadata DataBuilder Task";
+    public static final String OPERATOR_NAME_REFRESH = "Metadata DataBuilder Operator(Refresh)";
+    public static final String OPERATOR_NAME_BUILD_ALL = "Metadata DataBuilder Operator(Build All)";
+    public static final String TASK_NAME_REFRESH = "Metadata DataBuilder Task(Refresh)";
+    public static final String TASK_NAME_BUILD_ALL = "Metadata DataBuilder Task(Build All)";
     public static final String OPERATORS = "operators";
     public static final String TASKS = "tasks";
     public static final String TASKS_RUN = "tasks/_run";
-    public static final String PACKAGE_PATH = "file:/server/lib/kun-metadata-databuilder-1.0.jar";
+    public static final String PACKAGE_PATH_REFRESH = "file:/server/lib/kun-metadata-databuilder-1.0.jar";
+    public static final String PACKAGE_PATH_BUILD_ALL = "file:/server/lib/kun-metadata-databuilder-schema.jar";
     public static final String CLASS_NAME = "com.miotech.kun.metadata.databuilder.schedule.DataBuilderOperator";
 
 }
