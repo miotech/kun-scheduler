52b097a5735cd2ad6159964be7a6a487a226a021
jiechen <jie.chen@mioying.com>
2020-07-23 15:52:03 +0800

    ER: SP-4425 enhance dataset search

--- kun-data-quality/src/main/java/com/miotech/kun/dataquality/BeanConfig.java
+++ kun-data-quality/src/main/java/com/miotech/kun/dataquality/BeanConfig.java
@@ -4,13 +4,10 @@ import com.miotech.kun.dataquality.utils.WorkflowUtils;
 import com.miotech.kun.workflow.client.DefaultWorkflowClient;
 import com.miotech.kun.workflow.client.WorkflowClient;
 import com.miotech.kun.workflow.client.model.Operator;
-import com.miotech.kun.workflow.core.model.common.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 /**
  * @author: Jie Chen
@@ -32,13 +29,11 @@ public class BeanConfig {
 
     @Bean
     Operator getOperator() {
-        List<Param> params = workflowUtils.getInitParams("");
         return Operator.newBuilder()
                 .withName(DataQualityConfiguration.WORKFLOW_OPERATOR_NAME)
                 .withDescription("default operator")
                 .withClassName("com.miotech.kun.workflow.operator.DataQualityOperator")
                 .withPackagePath("file:/server/lib/data-quality-operators.jar")
-                .withParams(params)
                 .build();
     }
 }
