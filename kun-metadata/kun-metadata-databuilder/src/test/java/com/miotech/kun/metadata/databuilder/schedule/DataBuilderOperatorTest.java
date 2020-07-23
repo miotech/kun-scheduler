14723674fe1d65793f7c1643564d3199cc006393
zhangxinwei <xinwei.zhang@mioying.com>
2020-07-23 19:02:02 +0800

    Feature:SP-4402 reimport model

--- kun-metadata/kun-metadata-databuilder/src/test/java/com/miotech/kun/metadata/databuilder/schedule/DataBuilderOperatorTest.java
+++ kun-metadata/kun-metadata-databuilder/src/test/java/com/miotech/kun/metadata/databuilder/schedule/DataBuilderOperatorTest.java
@@ -1,7 +1,12 @@
 package com.miotech.kun.metadata.databuilder.schedule;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
 public class DataBuilderOperatorTest {
 
+    @Test
     public void testRun_lackOfVariable() {
         // TODO: need implementation
     }
