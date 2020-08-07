58fab25e5537c5c77ce9c9064c325256e50c2bbc
王晨 <wang.chen@mioying.com>
2020-08-07 16:24:16 +0800

    ER: fix some pdf bus SP-4509

--- kun-frontend/src/utils/index.ts
+++ kun-frontend/src/utils/index.ts
@@ -49,3 +49,8 @@ export const hasOptionalPermissions = (
   });
   return has;
 };
+
+// 判断是否支持全屏
+export const isFullscreenEnabled = () => document.fullscreenEnabled;
+// 判断是否全屏
+export const isFullscreen = () => document.fullscreenElement;
