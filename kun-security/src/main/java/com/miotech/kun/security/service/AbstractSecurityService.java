dff20330243542c01e2ea4e70853e7b74d123b7a
Melo <melo@mioying.com>
2020-08-28 11:53:13 +0800

    ER: Change gitignore. Cleanup repo history. Enhance user config.
    Signed-off-by: Melo <melo@mioying.com>

--- kun-security/src/main/java/com/miotech/kun/security/service/AbstractSecurityService.java
+++ kun-security/src/main/java/com/miotech/kun/security/service/AbstractSecurityService.java
@@ -3,34 +3,24 @@ package com.miotech.kun.security.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Sets;
 import com.miotech.kun.common.model.RequestResult;
-import com.miotech.kun.security.common.LdapUserIdAttributesMapper;
 import com.miotech.kun.security.model.bo.UserInfo;
 import com.miotech.kun.security.model.entity.User;
 import com.miotech.kun.security.util.Constants;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.ldap.LdapProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.ldap.core.AttributesMapper;
-import org.springframework.ldap.core.LdapTemplate;
-import org.springframework.ldap.filter.AndFilter;
-import org.springframework.ldap.filter.EqualsFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Service;
 
-import javax.naming.directory.SearchControls;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
-public class SecurityService implements InitializingBean {
+public class AbstractSecurityService implements InitializingBean {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -38,47 +28,6 @@ public class SecurityService implements InitializingBean {
 
     @Autowired
     UserService userService;
-
-    @Autowired
-    LdapTemplate ldapTemplate;
-
-    @Autowired
-    LdapProperties ldapProperties;
-
-    @Value("${security.ldap.user-group-search-base: ou=Groups}")
-    private String groupSearchBase;
-
-    public List<String> getUserGroup(String username) {
-        AndFilter filter = new AndFilter();
-        filter.and(new EqualsFilter("objectClass", "posixGroup"))
-                .and(new EqualsFilter("memberUid~", username));
-
-        return ldapTemplate.search(groupSearchBase,
-                filter.encode(),
-                SearchControls.SUBTREE_SCOPE,
-                (AttributesMapper<String>) attributes -> (String) attributes.get("cn").get());
-    }
-
-    public List<String> getUsersFromGroup(String group) {
-        String filter = new EqualsFilter("cn", group).encode();
-
-        return Arrays.asList(ldapTemplate.search(groupSearchBase,
-                filter,
-                SearchControls.SUBTREE_SCOPE,
-                new LdapUserIdAttributesMapper()).get(0).split(","));
-    }
-
-    public List<String> getSameGroupUsers(String username) {
-        Set<String> users = Sets.newHashSet();
-        Set<String> groups = this.getUserGroup(username).stream().collect(Collectors.toSet());
-        if(groups.contains(Constants.MIOTECH_USER_GROUP)) {
-            users.addAll(this.getUsersFromGroup(Constants.MIOTECH_USER_GROUP));
-        }
-        if(groups.contains(Constants.MOODYS_USER_GROUP)) {
-            users.addAll(this.getUsersFromGroup(Constants.MOODYS_USER_GROUP));
-        }
-        return users.stream().collect(Collectors.toList());
-    }
 
     public UserInfo saveUser(UserInfo userInfo) {
         User user = userService.addUser(userInfo);
@@ -124,8 +73,9 @@ public class SecurityService implements InitializingBean {
     }
 
     public UserInfo enrichUserInfo(UserInfo userInfo) {
-        List<String> userGroups = this.getUserGroup(userInfo.getUsername());
-        Set<String> permissions = this.getGroupPermission(userGroups);
+        Set<String> permissions = Sets.newHashSet();
+        permissions.add(Constants.PERMISSION_DATA_DISCOVERY);
+        permissions.add(Constants.PERMISSION_DATA_DEVELOPMENT);
         userInfo.setPermissions(permissions);
         return userInfo;
     }
@@ -156,19 +106,6 @@ public class SecurityService implements InitializingBean {
             response.setCharacterEncoding(Constants.HTTP_ENCODING);
             objectMapper.writeValue(response.getWriter(), RequestResult.success("Logout Successfully."));
         };
-    }
-
-    public Set<String> getGroupPermission(List<String> groups) {
-        Set<String> permissions = Sets.newHashSet();
-        Set<String> groupSet = groups.stream().collect(Collectors.toSet());
-        if(groupSet.contains(Constants.MOODYS_USER_GROUP)) {
-            permissions.add(Constants.PERMISSION_PDF_COA);
-        } else if(groupSet.contains(Constants.MIOTECH_USER_GROUP)) {
-            permissions.add(Constants.PERMISSION_DATA_DISCOVERY);
-            permissions.add(Constants.PERMISSION_DATA_DEVELOPMENT);
-            permissions.add(Constants.PERMISSION_PDF_GENERAL);
-        }
-        return permissions;
     }
 
     @Override
--- kun-security/src/main/java/com/miotech/kun/security/util/Constants.java
+++ kun-security/src/main/java/com/miotech/kun/security/util/Constants.java
@@ -6,17 +6,9 @@ package com.miotech.kun.security.util;
  */
 public class Constants {
 
-    final public static String HTTP_CONTENT_TYPE = "application/json";
-    final public static String HTTP_ENCODING = "utf-8";
+    public static final String HTTP_CONTENT_TYPE = "application/json";
+    public static final String HTTP_ENCODING = "utf-8";
 
-    final public static String MOODYS_USER_GROUP = "moody's";
-    final public static String MIOTECH_USER_GROUP = "miotech";
-
-    final public static String PERMISSION_DATA_DISCOVERY = "DATA_DISCOVERY";
-    final public static String PERMISSION_DATA_DEVELOPMENT = "DATA_DEVELOPMENT";
-
-    final public static String PERMISSION_PDF_GENERAL = "PDF_GENERAL";
-    final public static String PERMISSION_PDF_COA = "PDF_COA";
-
-    public final static String[] EMPTY_PERSSIONS = new String[]{};
+    public static final String PERMISSION_DATA_DISCOVERY = "DATA_DISCOVERY";
+    public static final String PERMISSION_DATA_DEVELOPMENT = "DATA_DEVELOPMENT";
 }
