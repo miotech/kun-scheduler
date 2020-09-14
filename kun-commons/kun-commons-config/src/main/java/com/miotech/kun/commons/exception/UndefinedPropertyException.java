d34cba157bc9316a5b530dd139921d8c13aca41e
Junpeng Ouyang <josh.ouyang@mioying.com>
2020-09-14 15:24:10 +0800

    fix(data-lineage): remove classes & configurations that not in scope

--- kun-commons/kun-commons-config/src/main/java/com/miotech/kun/commons/exception/UndefinedPropertyException.java
+++
@@ -1,7 +0,0 @@
-package com.miotech.kun.commons.exception;
-
-public class UndefinedPropertyException extends RuntimeException {
-    public UndefinedPropertyException(String s) {
-        super(s);
-    }
-}
