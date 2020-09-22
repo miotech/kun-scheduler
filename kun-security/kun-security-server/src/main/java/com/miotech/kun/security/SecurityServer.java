45335419144ec76ce3dead8a0df632d86ebc46f8
jiechen <jie.chen@mioying.com>
2020-09-22 20:15:44 +0800

    Refactor Security Module
    Closes #74

---
+++ kun-security/kun-security-server/src/main/java/com/miotech/kun/security/SecurityServer.java
@@ -0,0 +1,20 @@
+package com.miotech.kun.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ * @author: Jie Chen
+ * @created: 2020/9/22
+ */
+@SpringBootApplication
+@ComponentScan(excludeFilters  = {@ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE, classes = {SecurityConfig.class})})
+public class SecurityServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServer.class);
+    }
+}
