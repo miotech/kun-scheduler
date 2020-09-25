b57bdd0a998425c7caba8d7546a6876e4ae10593
zhangxinwei <xinwei.zhang@mioying.com>
2020-09-25 16:34:04 +0800

    Implemented a universal DataXOperator for data transmission
    Closes #85

---
+++ kun-workflow/kun-workflow-operators/datax-operator/src/main/java/com/miotech/kun/workflow/operator/constant/OperatorConfigKey.java
@@ -0,0 +1,16 @@
+package com.miotech.kun.workflow.operator.constant;
+
+public class OperatorConfigKey {
+
+    private OperatorConfigKey() {
+    }
+
+    public static final String JOB_JSON = "jobJson";
+
+    public static final String SOURCE_TABLE = "sourceTable";
+    public static final String SOURCE_TABLE_GID = "sourceTableGid";
+
+    public static final String TARGET_TABLE = "targetTable";
+    public static final String TARGET_TABLE_GID = "targetTableGid";
+
+}
