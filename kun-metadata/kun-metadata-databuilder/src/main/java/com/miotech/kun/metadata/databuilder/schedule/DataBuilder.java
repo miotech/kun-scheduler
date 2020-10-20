58ad100354193253a55e7604e75b54943fa0f8eb
zhangxinwei <xinwei.zhang@mioying.com>
2020-10-20 14:35:09 +0800

    The corresponding datasets is deleted when the datasource connection fails Closes #162

--- kun-metadata/kun-metadata-databuilder/src/main/java/com/miotech/kun/metadata/databuilder/schedule/DataBuilder.java
+++ kun-metadata/kun-metadata-databuilder/src/main/java/com/miotech/kun/metadata/databuilder/schedule/DataBuilder.java
@@ -3,7 +3,6 @@ package com.miotech.kun.metadata.databuilder.schedule;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.miotech.kun.commons.db.DatabaseOperator;
 import com.miotech.kun.commons.utils.ExceptionUtils;
 import com.miotech.kun.commons.utils.Props;
@@ -38,41 +37,24 @@ import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Singleton
 public class DataBuilder {
-
     private static final Logger logger = LoggerFactory.getLogger(DataBuilder.class);
 
-    private final AtomicBoolean scheduled;
-
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(
-            1,
-            new ThreadFactoryBuilder().setNameFormat("data-builder-scheduler").build()
-    );
-
     private final ExecutorService threadPool = Executors.newFixedThreadPool(5);
-
     private final DatabaseOperator operator;
-
     private final Loader loader;
-
-    private Map<String, Long> latestStates = Maps.newConcurrentMap();
-
     private final Props props;
 
     @Inject
     public DataBuilder(DatabaseOperator operator, Props props) {
-        this.scheduled = new AtomicBoolean(false);
         this.operator = operator;
         this.loader = new PostgresLoader(operator);
         this.props = props;
-    }
-
-    public Map<String, Long> getLatestStates() {
-        return latestStates;
     }
 
     public void buildAll() {
@@ -82,13 +64,14 @@ public class DataBuilder {
         for (DataSource dataSource : dataSources) {
             threadPool.submit(() -> {
                 try {
-                    build(dataSource);
+                    Map<String, Long> latestStates = Maps.newHashMap();
+                    build(dataSource, latestStates);
+                    sweep(latestStates);
                 } catch (Exception e) {
                     logger.error("DataBuilder buildAll build fail, dataSource: {}", JSONUtils.toJsonString(dataSource), e);
                 } finally {
                     countDownLatch.countDown();
                 }
-
             });
         }
         try {
@@ -100,20 +83,34 @@ public class DataBuilder {
     }
 
     public void buildDatasource(long datasourceId) {
-        Preconditions.checkArgument(datasourceId > 0L, "datasourceId must be a positive long, datasourceId: %s", datasourceId);
+        try {
+            Preconditions.checkArgument(datasourceId > 0L, "datasourceId must be a positive long, datasourceId: %s", datasourceId);
 
-        String sql = "SELECT kmd.id, kmdt.name, kmd.connection_info FROM kun_mt_datasource kmd JOIN kun_mt_datasource_type kmdt ON kmd.type_id = kmdt.id WHERE kmd.id = ?";
-        DataSource dataSource = operator.fetchOne(sql, rs -> generateDataSource(rs.getLong(1), rs.getString(2), rs.getString(3)), datasourceId);
-        build(dataSource);
+            String sql = "SELECT kmd.id, kmdt.name, kmd.connection_info FROM kun_mt_datasource kmd JOIN kun_mt_datasource_type kmdt ON kmd.type_id = kmdt.id WHERE kmd.id = ?";
+            DataSource dataSource = operator.fetchOne(sql, rs -> generateDataSource(rs.getLong(1), rs.getString(2), rs.getString(3)), datasourceId);
+            Map<String, Long> latestStates = Maps.newHashMap();
+            build(dataSource, latestStates);
+            sweep(latestStates);
+        } catch (Exception e) {
+            logger.error("DataBuilder buildDatasource build fail, dataSourceId: {}", datasourceId, e);
+            throw ExceptionUtils.wrapIfChecked(e);
+        }
     }
 
     public void buildDataset(Long gid) {
-        Long datasourceId = operator.fetchOne("SELECT datasource_id FROM kun_mt_dataset WHERE gid = ?", rs -> rs.getLong(1), gid);
-        Preconditions.checkNotNull(datasourceId, "Invalid param `gid`: " + gid + " No corresponding datasource found");
+        try {
+            Long datasourceId = operator.fetchOne("SELECT datasource_id FROM kun_mt_dataset WHERE gid = ?", rs -> rs.getLong(1), gid);
+            Preconditions.checkNotNull(datasourceId, "Invalid param `gid`: " + gid + " No corresponding datasource found");
 
-        String sql = "SELECT kmds.id, kmdst.name, kmds.connection_info, kmd.data_store, kmd.gid FROM kun_mt_dataset kmd JOIN kun_mt_datasource kmds ON kmd.datasource_id = kmds.id JOIN kun_mt_datasource_type kmdst ON kmds.type_id = kmdst.id WHERE kmd.gid = ?";
-        DatasetConnDto datasetConnDto = operator.fetchOne(sql, this::buildDatasetConnDto, gid);
-        build(datasetConnDto);
+            String sql = "SELECT kmds.id, kmdst.name, kmds.connection_info, kmd.data_store, kmd.gid FROM kun_mt_dataset kmd JOIN kun_mt_datasource kmds ON kmd.datasource_id = kmds.id JOIN kun_mt_datasource_type kmdst ON kmds.type_id = kmdst.id WHERE kmd.gid = ?";
+            DatasetConnDto datasetConnDto = operator.fetchOne(sql, this::buildDatasetConnDto, gid);
+            Map<String, Long> latestStates = Maps.newHashMap();
+            build(datasetConnDto, latestStates);
+            sweep(latestStates);
+        } catch (Exception e) {
+            logger.error("DataBuilder buildDataset build fail, gid: {}", gid, e);
+            throw ExceptionUtils.wrapIfChecked(e);
+        }
     }
 
     private DataSource generateDataSource(long id, String datasourceType, String connStr) throws SQLException {
@@ -183,104 +180,87 @@ public class DataBuilder {
         return datasetConnDtoBuilder.build();
     }
 
-    private void build(DatasetConnDto datasetConnDto) {
+    private void build(DatasetConnDto datasetConnDto, Map<String, Long> latestStates) {
         Preconditions.checkNotNull(datasetConnDto, "datasetConnDto should not be null.");
-        recordStates(datasetConnDto.getDataStore(), datasetConnDto.getGid());
-        try {
-            DataSource dataSource = datasetConnDto.getDataSource();
-            DataStore dataStore = datasetConnDto.getDataStore();
-            Dataset dataset = null;
-            if (dataSource instanceof AWSDataSource) {
-                AWSDataSource awsDataSource = (AWSDataSource) dataSource;
-                HiveTableStore hiveTableStore = (HiveTableStore) dataStore;
-                dataset = new GlueTableExtractor(props, awsDataSource, GlueClient.searchTable(awsDataSource,
-                        hiveTableStore.getDatabase(), hiveTableStore.getTable())).extract().next();
-            } else if (dataSource instanceof PostgresDataSource) {
-                PostgresDataSource pgDataSource = (PostgresDataSource) dataSource;
-                PostgresDataStore pgDataStore = (PostgresDataStore) dataStore;
-                dataset = new PostgresTableExtractor(props, pgDataSource,
-                        pgDataStore.getDatabase(),
-                        pgDataStore.getSchema(),
-                        pgDataStore.getTableName()
-                ).extract().next();
-            } else if (dataSource instanceof MongoDataSource) {
-                dataset = new MongoCollectionExtractor(props, ((MongoDataSource) dataSource),
-                        ((MongoDataStore) dataStore).getDatabase(),
-                        ((MongoDataStore) dataStore).getCollection()
-                ).extract().next();
-            } else if (dataSource instanceof ElasticSearchDataSource) {
-                ElasticSearchDataSource elasticSearchDataSource = (ElasticSearchDataSource) dataSource;
-                dataset = new ElasticSearchIndexExtractor(props, elasticSearchDataSource,
-                        ((ElasticSearchIndexStore) dataStore).getIndex()
-                ).extract().next();
-            } else if (dataSource instanceof ArangoDataSource) {
-                dataset = new ArangoCollectionExtractor(props, ((ArangoDataSource) dataSource),
-                        ((ArangoCollectionStore) dataStore).getDatabase(),
-                        ((ArangoCollectionStore) dataStore).getCollection()
-                ).extract().next();
-            }
+        recordStates(datasetConnDto.getDataStore(), datasetConnDto.getGid(), latestStates);
+        DataSource dataSource = datasetConnDto.getDataSource();
+        DataStore dataStore = datasetConnDto.getDataStore();
+        Dataset dataset = null;
+        if (dataSource instanceof AWSDataSource) {
+            AWSDataSource awsDataSource = (AWSDataSource) dataSource;
+            HiveTableStore hiveTableStore = (HiveTableStore) dataStore;
+            dataset = new GlueTableExtractor(props, awsDataSource, GlueClient.searchTable(awsDataSource,
+                    hiveTableStore.getDatabase(), hiveTableStore.getTable())).extract().next();
+        } else if (dataSource instanceof PostgresDataSource) {
+            PostgresDataSource pgDataSource = (PostgresDataSource) dataSource;
+            PostgresDataStore pgDataStore = (PostgresDataStore) dataStore;
+            dataset = new PostgresTableExtractor(props, pgDataSource,
+                    pgDataStore.getDatabase(),
+                    pgDataStore.getSchema(),
+                    pgDataStore.getTableName()
+            ).extract().next();
+        } else if (dataSource instanceof MongoDataSource) {
+            dataset = new MongoCollectionExtractor(props, ((MongoDataSource) dataSource),
+                    ((MongoDataStore) dataStore).getDatabase(),
+                    ((MongoDataStore) dataStore).getCollection()
+            ).extract().next();
+        } else if (dataSource instanceof ElasticSearchDataSource) {
+            ElasticSearchDataSource elasticSearchDataSource = (ElasticSearchDataSource) dataSource;
+            dataset = new ElasticSearchIndexExtractor(props, elasticSearchDataSource,
+                    ((ElasticSearchIndexStore) dataStore).getIndex()
+            ).extract().next();
+        } else if (dataSource instanceof ArangoDataSource) {
+            dataset = new ArangoCollectionExtractor(props, ((ArangoDataSource) dataSource),
+                    ((ArangoCollectionStore) dataStore).getDatabase(),
+                    ((ArangoCollectionStore) dataStore).getCollection()
+            ).extract().next();
+        }
 
-            try {
-                loader.load(dataset);
-                mark(dataset);
-            } catch (Exception e) {
-                logger.error("load error: ", e);
-            }
+        try {
+            loader.load(dataset);
+            mark(dataset, latestStates);
         } catch (Exception e) {
-            logger.error("build dataset error: ", e);
+            logger.error("load error: ", e);
         }
     }
 
 
-    private void build(DataSource dataSource) {
+    private void build(DataSource dataSource, Map<String, Long> latestStates) {
         Preconditions.checkNotNull(dataSource, "dataSource should not be null.");
-        recordStates(dataSource.getId());
-        try {
-            Iterator<Dataset> datasetIterator = null;
-            if (dataSource instanceof AWSDataSource) {
-                datasetIterator = new GlueExtractor(props, (AWSDataSource) dataSource).extract();
-            } else if (dataSource instanceof PostgresDataSource) {
-                datasetIterator = new PostgresExtractor(props, (PostgresDataSource) dataSource).extract();
-            } else if (dataSource instanceof MongoDataSource) {
-                datasetIterator = new MongoExtractor(props, (MongoDataSource) dataSource).extract();
-            } else if (dataSource instanceof ElasticSearchDataSource) {
-                ElasticSearchDataSource elasticSearchDataSource = (ElasticSearchDataSource) dataSource;
-                datasetIterator = new ElasticsearchExtractor(props, elasticSearchDataSource).extract();
-            } else if (dataSource instanceof ArangoDataSource) {
-                datasetIterator = new ArangoExtractor(props, (ArangoDataSource) dataSource).extract();
-            }
+        recordStates(dataSource.getId(), latestStates);
+        Iterator<Dataset> datasetIterator = null;
+        if (dataSource instanceof AWSDataSource) {
+            datasetIterator = new GlueExtractor(props, (AWSDataSource) dataSource).extract();
+        } else if (dataSource instanceof PostgresDataSource) {
+            datasetIterator = new PostgresExtractor(props, (PostgresDataSource) dataSource).extract();
+        } else if (dataSource instanceof MongoDataSource) {
+            datasetIterator = new MongoExtractor(props, (MongoDataSource) dataSource).extract();
+        } else if (dataSource instanceof ElasticSearchDataSource) {
+            ElasticSearchDataSource elasticSearchDataSource = (ElasticSearchDataSource) dataSource;
+            datasetIterator = new ElasticsearchExtractor(props, elasticSearchDataSource).extract();
+        } else if (dataSource instanceof ArangoDataSource) {
+            datasetIterator = new ArangoExtractor(props, (ArangoDataSource) dataSource).extract();
+        }
 
-            if (datasetIterator != null) {
-                while (datasetIterator.hasNext()) {
-                    try {
-                        Dataset dataset = datasetIterator.next();
-                        loader.load(dataset);
+        if (datasetIterator != null) {
+            while (datasetIterator.hasNext()) {
+                try {
+                    Dataset dataset = datasetIterator.next();
+                    loader.load(dataset);
 
-                        mark(dataset);
-                    } catch (Exception e) {
-                        logger.error("etl next error: ", e);
-                    }
+                    mark(dataset, latestStates);
+                } catch (Exception e) {
+                    logger.error("etl next error: ", e);
                 }
             }
-        } catch (Exception e) {
-            logger.error("start etl error: ", e);
         }
     }
 
-    public void scheduleAtRate(long initialDelay, long period, TimeUnit unit) {
-        if (scheduled.compareAndSet(false, true)) {
-            logger.info("Start scheduling buildAll task. period={}, unit={}", period, unit);
-            scheduler.scheduleAtFixedRate(this::buildAll, initialDelay, period, unit);
-        } else {
-            throw new IllegalStateException("BuildAll task is already scheduled.");
-        }
-    }
-
-    private void recordStates(long datasourceId) {
+    private void recordStates(long datasourceId, Map<String, Long> latestStates) {
         operator.fetchAll("SELECT data_store, gid FROM kun_mt_dataset WHERE datasource_id = ?",
                 rs -> {
                     try {
-                        latestStates.put(DataStoreJsonUtil.toJson(DataStoreJsonUtil.toDataStore(rs.getString(1))), rs.getLong(2));
+                        recordStates(DataStoreJsonUtil.toDataStore(rs.getString(1)), rs.getLong(2), latestStates);
                     } catch (JsonProcessingException jsonProcessingException) {
                         throw ExceptionUtils.wrapIfChecked(jsonProcessingException);
                     }
@@ -288,7 +268,7 @@ public class DataBuilder {
                 }, datasourceId);
     }
 
-    private void recordStates(DataStore dataStore, long gid) {
+    private void recordStates(DataStore dataStore, long gid, Map<String, Long> latestStates) {
         try {
             latestStates.put(DataStoreJsonUtil.toJson(dataStore), gid);
         } catch (JsonProcessingException jsonProcessingException) {
@@ -296,7 +276,7 @@ public class DataBuilder {
         }
     }
 
-    private void mark(Dataset dataset) throws JsonProcessingException {
+    private void mark(Dataset dataset, Map<String, Long> latestStates) throws JsonProcessingException {
         if (dataset.getDataStore() == null) {
             return;
         }
@@ -307,7 +287,7 @@ public class DataBuilder {
         }
     }
 
-    public void sweep() {
+    private void sweep(Map<String, Long> latestStates) {
         if (!latestStates.isEmpty()) {
             logger.info("prepare to delete dataset: {}", JSONUtils.toJsonString(latestStates.values()));
             Object[][] params = latestStates.values().stream().map(gid -> new Object[]{gid}).toArray(Object[][]::new);
