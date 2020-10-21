d82d6a37596f861f7bc512041f856ca51f3e3a65
Melo <melo@mioying.com>
2020-10-21 15:27:31 +0800

    Add Druid Auto-Pop Logic Enhance the data quality associated dataset logic.
    Closes #171

--- kun-data-quality/src/main/java/com/miotech/kun/dataquality/utils/Constants.java
+++ kun-data-quality/src/main/java/com/miotech/kun/dataquality/utils/Constants.java
@@ -1,5 +1,10 @@
 package com.miotech.kun.dataquality.utils;
 
+import com.alibaba.druid.util.JdbcConstants;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
 /**
  * @author: Melo
  * @created: 8/13/20
@@ -9,4 +14,12 @@ public class Constants {
     final public static String DQ_RESULT_PREFIX = "DQ_RECORD>>>";
     final public static String DQ_RESULT_SUCCESS = "SUCCESS";
     final public static String DQ_RESULT_FAILED = "FAILED";
+
+    final public static Map<String, String> DATASOURCE_TO_DRUID_TYPE = Maps.newHashMap();
+
+    static {
+        DATASOURCE_TO_DRUID_TYPE.put("AWS", JdbcConstants.HIVE);
+        DATASOURCE_TO_DRUID_TYPE.put("PostgreSQL", JdbcConstants.POSTGRESQL);
+        DATASOURCE_TO_DRUID_TYPE.put("Elasticsearch", JdbcConstants.ELASTIC_SEARCH);
+    }
 }
