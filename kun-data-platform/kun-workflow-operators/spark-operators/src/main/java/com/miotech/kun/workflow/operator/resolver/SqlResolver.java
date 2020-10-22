7319de895297ad9d82e613535f0e3bb9084b1d4c
shikimoe <w_yide@163.com>
2020-10-22 14:21:51 +0800

    fix conflict

--- kun-data-platform/kun-workflow-operators/spark-operators/src/main/java/com/miotech/kun/workflow/operator/resolver/SqlResolver.java
+++ kun-data-platform/kun-workflow-operators/spark-operators/src/main/java/com/miotech/kun/workflow/operator/resolver/SqlResolver.java
@@ -69,6 +69,9 @@ public abstract class SqlResolver implements Resolver {
         if (tableName.contains("`")) {
             tableName = tableName.replaceAll("`", "");
         }
+        if (tableName.contains("'")) {
+            tableName = tableName.replaceAll("'", "");
+        }
         if (tableName.contains("\"")) {
             tableName = tableName.replaceAll("\"", "");
         }
