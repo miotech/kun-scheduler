1c9cb824b8837e013d61610ee0bbf709ebdea2f4
jiechen <jie.chen@mioying.com>
2020-10-29 17:38:50 +0800

    enhance dash board test cases content and lineage bug fix
    Closes #207

--- kun-data-quality/data-quality-operators/src/main/java/com/miotech/kun/workflow/operator/model/DataQualityCaseMetrics.java
+++ kun-data-quality/data-quality-operators/src/main/java/com/miotech/kun/workflow/operator/model/DataQualityCaseMetrics.java
@@ -2,6 +2,9 @@ package com.miotech.kun.workflow.operator.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: Jie Chen
  * @created: 2020/9/27
@@ -14,4 +17,10 @@ public class DataQualityCaseMetrics {
     String errorReason;
 
     CaseStatus caseStatus;
+
+    List<DataQualityRule> ruleRecords = new ArrayList<>();
+
+    public void add(DataQualityRule ruleRecord) {
+        ruleRecords.add(ruleRecord);
+    }
 }
