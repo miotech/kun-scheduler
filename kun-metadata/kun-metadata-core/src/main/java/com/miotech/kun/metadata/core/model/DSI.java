0ff259e75081d12f721b750f753b10e07e4969b4
Junpeng Ouyang <josh.ouyang@mioying.com>
2020-10-30 14:09:07 +0800

    feat(metadata-dsi): implement DataStore Identifier (DSI) class and add test cases

---
+++ kun-metadata/kun-metadata-core/src/main/java/com/miotech/kun/metadata/core/model/DSI.java
@@ -0,0 +1,253 @@
+package com.miotech.kun.metadata.core.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.miotech.kun.commons.utils.ExceptionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * <b>Data Store Identifier</b> (DSI) class encodes information of a datastore
+ * The encoding structure is:
+ * <code>&lt;protocol&gt;[:&lt;props1=value1,props2=value2,...&gt;[:extra1=extraValue1,extra2=extraValue2,...]]</code>
+ *
+ * @author Josh Ouyang
+ */
+public class DSI implements Serializable {
+    @JsonIgnore
+    private static final long serialVersionUID = -1604023597334L;
+
+    private static final String PROP_KEY_VALUE_PATTERN = "(?:(?:[a-zA-Z0-9_]|[^\\w,:%]|(?:%[0-9a-fA-F]+))+)";
+
+    private static final Pattern validatePattern =
+            Pattern.compile("^([a-zA-Z0-9_]+)" +    // store type
+                    "(?:" +
+                    ":" + "(?:(" + PROP_KEY_VALUE_PATTERN + "=" + PROP_KEY_VALUE_PATTERN + "(?:," + PROP_KEY_VALUE_PATTERN + "=" + PROP_KEY_VALUE_PATTERN + ")*)?)" +  // properties
+                    "(?::(" + PROP_KEY_VALUE_PATTERN + "=" + PROP_KEY_VALUE_PATTERN + "(?:," + PROP_KEY_VALUE_PATTERN + "=" + PROP_KEY_VALUE_PATTERN + ")*))?" +  // extras
+                    ")?$");
+
+    private transient String dsiString;
+
+    private final String storeType;
+
+    private final ImmutableMap<String, String> props;
+
+    private final ImmutableMap<String, String> extras;
+
+    public String getStoreType() {
+        return storeType;
+    }
+
+    public Map<String, String> getProps() {
+        return props;
+    }
+
+    public Map<String, String> getExtras() {
+        return extras;
+    }
+
+    private DSI(DSIBuilder builder) {
+        Preconditions.checkNotNull(builder);
+        Preconditions.checkArgument(StringUtils.isNotBlank(builder.storeType), "storeType cannot be blank when building dsi");
+
+        this.storeType = builder.storeType;
+        this.props = ImmutableMap.copyOf(builder.props);
+        this.extras = ImmutableMap.copyOf(builder.extras);
+    }
+
+    public static DSIBuilder newBuilder() {
+        return new DSIBuilder();
+    }
+
+    public DSIBuilder cloneBuilder() {
+        DSIBuilder builder = new DSIBuilder();
+        builder.withStoreType(this.storeType);
+        for (Map.Entry<String, String> entry : this.props.entrySet()) {
+            builder.putProperty(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, String> entry : this.extras.entrySet()) {
+            builder.putExtra(entry.getKey(), entry.getValue());
+        }
+        return builder;
+    }
+
+    public static DSI from(String string) {
+        Matcher matcher = validatePattern.matcher(string);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(String.format("Cannot build DSI from invalid string: \"%s\"", string));
+        }
+        String storeType = matcher.group(1);
+        String propsStr = matcher.group(2);
+        String extraStr = matcher.group(3);
+
+        Map<String, String> propsMap = transformEncodedStringToMap(propsStr);
+        Map<String, String> extraMap = transformEncodedStringToMap(extraStr);
+
+        return new DSIBuilder()
+                .withStoreType(storeType)
+                .withProps(propsMap)
+                .withExtras(extraMap)
+                .build();
+    }
+
+    private static Map<String, String> transformEncodedStringToMap(String encodedString) {
+        Map<String, String> props = new HashMap<>();
+        if (StringUtils.isBlank(encodedString)) {
+            return props;
+        }
+        String[] kvPairs = encodedString.split(",");
+        for (String kvPair : kvPairs) {
+            String[] pair = kvPair.split("=");
+            String key = pair[0];
+            String value = pair[1];
+            if (props.containsKey(key)) {
+                throw new IllegalArgumentException(String.format("Duplicated key in property pair: %s", kvPair));
+            }
+            // else
+            props.put(decodeString(key), decodeString(value));
+        }
+        return props;
+    }
+
+    /**
+     * Decodes string from {@code application/x-www-form-urlencoded}
+     * format using a specific encoding scheme.
+     * @param str string in format of application/x-www-form-urlencoded
+     * @return decoded string
+     */
+    private static String decodeString(String str) {
+        try {
+            return URLDecoder.decode(str, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(ExceptionUtils.wrapIfChecked(e));
+        }
+    }
+
+    /**
+     * Convert DSI to URL-encoded format string
+     * @return encoded DSI string
+     */
+    @Override
+    public String toString() {
+        // lazy evaluate and cache the urn string since it is immutable
+        if (StringUtils.isBlank(this.dsiString)) {
+            // if dsiString is not initialized
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(encodeString(storeType));
+            if (!props.keySet().isEmpty()) {
+                stringBuilder.append(":");
+                stringBuilder.append(mapToEncodedString(props));
+            }
+            if (!extras.keySet().isEmpty()) {
+                if (props.keySet().isEmpty()) {
+                    stringBuilder.append("::");
+                } else {
+                    stringBuilder.append(":");
+                }
+                stringBuilder.append(mapToEncodedString(extras));
+            }
+            dsiString = stringBuilder.toString();
+        }
+        return dsiString;
+    }
+
+    /**
+     * Translates a string into {@code application/x-www-form-urlencoded}
+     * format using a specific encoding scheme.
+     * @param str string in raw format
+     * @return encoded string in application/x-www-form-urlencoded scheme
+     */
+    private static String encodeString(String str) {
+        try {
+            return URLEncoder.encode(str, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(ExceptionUtils.wrapIfChecked(e));
+        }
+    }
+
+    /**
+     * converts properties/extras map to encoded form url scheme string.
+     * @param props key-value map
+     * @return encoded map string
+     */
+    private static String mapToEncodedString(Map<String, String> props) {
+        StringBuilder stringBuilder = new StringBuilder();
+        List<String> sortedKeyList = props.keySet().stream().sorted().collect(Collectors.toList());
+        for (String key : sortedKeyList) {
+            stringBuilder
+                    .append(encodeString(key))
+                    .append('=')
+                    .append(encodeString(props.get(key)))
+                    .append(',');
+        }
+        // remove last comma
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DSI dsi = (DSI) o;
+        return Objects.equals(storeType, dsi.storeType) &&
+                Objects.equals(props, dsi.props) &&
+                Objects.equals(extras, dsi.extras);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(storeType, props, extras);
+    }
+
+    public static final class DSIBuilder {
+        private String storeType;
+        private Map<String, String> props = new HashMap<>();
+        private Map<String, String> extras = new HashMap<>();
+
+        private DSIBuilder() {
+        }
+
+        public DSIBuilder withStoreType(String storeType) {
+            this.storeType = storeType;
+            return this;
+        }
+
+        public DSIBuilder withProps(Map<String, String> props) {
+            this.props = props;
+            return this;
+        }
+
+        public DSIBuilder withExtras(Map<String, String> extras) {
+            this.extras = extras;
+            return this;
+        }
+
+        public DSIBuilder putProperty(String key, String value) {
+            this.props.put(key, value);
+            return this;
+        }
+
+        public DSIBuilder putExtra(String key, String value) {
+            this.extras.put(key, value);
+            return this;
+        }
+
+        public DSI build() {
+            return new DSI(this);
+        }
+    }
+}
---
+++ kun-metadata/kun-metadata-core/src/test/java/com/miotech/kun/metadata/core/model/DSITest.java
@@ -0,0 +1,176 @@
+package com.miotech.kun.metadata.core.model;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class DSITest {
+    @Test
+    public void DSI_buildFromValidString_shouldCreateProperly() {
+        // Prepare
+        String dsiString1 = "mysql:host=db-server,port=3306,db=test,table=company";
+        String dsiString2 = "postgres:host=192.168.1.2,port=5432,db=test,schema=dm,table=company";
+        String dsiString3 = "file:path=/data/bin/abc.bin:stream=true,stderr=/logs/error.log";
+        String dsiString4 = "ssh:host=192.168.1.5";
+        String dsiString5 = "randomType::abc=def";
+
+        // Process
+        DSI dsi1 = DSI.from(dsiString1);
+        DSI dsi2 = DSI.from(dsiString2);
+        DSI dsi3 = DSI.from(dsiString3);
+        DSI dsi4 = DSI.from(dsiString4);
+        DSI dsi5 = DSI.from(dsiString5);
+
+        // Validate
+        assertThat(dsi1.getStoreType(), is("mysql"));
+        assertThat(dsi2.getStoreType(), is("postgres"));
+        assertThat(dsi3.getStoreType(), is("file"));
+        assertThat(dsi4.getStoreType(), is("ssh"));
+        assertThat(dsi5.getStoreType(), is("randomType"));
+
+        assertThat(dsi1.getProps().size(), is(4));
+        assertThat(dsi1.getExtras().size(), is(0));
+        assertThat(dsi1.getProps().get("host"), is("db-server"));
+        assertThat(dsi1.getProps().get("port"), is("3306"));
+        assertThat(dsi1.getProps().get("db"), is("test"));
+        assertThat(dsi1.getProps().get("table"), is("company"));
+
+        assertThat(dsi2.getProps().size(), is(5));
+        assertThat(dsi2.getExtras().size(), is(0));
+        assertThat(dsi2.getProps().get("host"), is("192.168.1.2"));
+        assertThat(dsi2.getProps().get("port"), is("5432"));
+        assertThat(dsi2.getProps().get("db"), is("test"));
+        assertThat(dsi2.getProps().get("schema"), is("dm"));
+        assertThat(dsi2.getProps().get("table"), is("company"));
+
+        assertThat(dsi3.getProps().size(), is(1));
+        assertThat(dsi3.getExtras().size(), is(2));
+        assertThat(dsi3.getProps().get("path"), is("/data/bin/abc.bin"));
+        assertThat(dsi3.getExtras().get("stream"), is("true"));
+        assertThat(dsi3.getExtras().get("stderr"), is("/logs/error.log"));
+
+        assertThat(dsi4.getProps().size(), is(1));
+        assertThat(dsi4.getExtras().size(), is(0));
+        assertThat(dsi4.getProps().get("host"), is("192.168.1.5"));
+
+        assertThat(dsi5.getProps().size(), is(0));
+        assertThat(dsi5.getExtras().size(), is(1));
+        assertThat(dsi5.getExtras().get("abc"), is("def"));
+
+        // when reverse convert to string, dsi should sort property keys in lexicographical order,
+        // and special characters should be encoded properly.
+        assertThat(dsi1.toString(), is("mysql:db=test,host=db-server,port=3306,table=company"));
+        assertThat(dsi2.toString(), is("postgres:db=test,host=192.168.1.2,port=5432,schema=dm,table=company"));
+        assertThat(dsi3.toString(), is("file:path=%2Fdata%2Fbin%2Fabc.bin:stderr=%2Flogs%2Ferror.log,stream=true"));
+        assertThat(dsi4.toString(), is("ssh:host=192.168.1.5"));
+        assertThat(dsi5.toString(), is("randomType::abc=def"));
+    }
+
+    @Test
+    public void DSI_buildFromStringWithURIEncodedCharacters_shouldParseProperly() {
+        // Prepare
+        String dsiStringEncoded1 = "https:host=localhost,key1=value+1,key2=value%40%21%242,key3=value%253%2c4";
+        String dsiStringEncoded2 = "ssh:host=%3a%3affff%3a0.0.0.0";
+
+        // Process
+        DSI dsi1 = DSI.from(dsiStringEncoded1);
+        DSI dsi2 = DSI.from(dsiStringEncoded2);
+
+        // Validate
+        Map<String, String> dsi1Props = dsi1.getProps();
+        assertThat(dsi1Props.get("host"), is("localhost"));
+        assertThat(dsi1Props.get("key1"), is("value 1"));
+        assertThat(dsi1Props.get("key2"), is("value@!$2"));
+        assertThat(dsi1Props.get("key3"), is("value%3,4"));
+
+        assertThat(dsi2.getProps().get("host"), is("::ffff:0.0.0.0"));
+
+        // when convert to string, special characters should be encoded
+        assertThat(dsi1.toString(), is("https:host=localhost,key1=value+1,key2=value%40%21%242,key3=value%253%2C4"));
+        assertThat(dsi2.toString(), is("ssh:host=%3A%3Affff%3A0.0.0.0"));
+    }
+
+    @Test
+    public void DSI_buildFromBuilder_shouldEquivalentToBuildFromString() {
+        // Prepare
+        DSI.DSIBuilder builder1 = DSI.newBuilder().withStoreType("mongodb");
+        builder1.putProperty("host", "127.0.0.1");
+        builder1.putProperty("port", "27017");
+        builder1.putProperty("db", "test");
+        builder1.putProperty("collection", "company");
+        DSI dsi1 = builder1.build();
+
+        DSI.DSIBuilder builder2 = DSI.newBuilder().withStoreType("file");
+        builder2.putExtra("name", "\"foobar.txt\"");
+        DSI dsi2 = builder2.build();
+
+        DSI dsiFromString1 = DSI.from("mongodb:host=127.0.0.1,port=27017,db=test,collection=company");
+        DSI dsiFromString2 = DSI.from("file::name=\"foobar.txt\"");
+
+        // Validate
+        assertEquals(dsi1, dsiFromString1);
+        assertEquals("mongodb:collection=company,db=test,host=127.0.0.1,port=27017", dsiFromString1.toString());
+        assertEquals(dsi1.toString(), dsiFromString1.toString());
+
+        assertEquals(dsi2, dsiFromString2);
+        assertEquals("file::name=%22foobar.txt%22", dsiFromString2.toString());
+        assertEquals(dsi2.toString(), dsiFromString2.toString());
+    }
+
+    @Test
+    public void DSI_buildFromInvalidString_shouldThrowException() {
+        // Prepare
+        String[] dsiStringsWithInvalidPattern = {
+                ":host=127.0.0.1",    // no store type
+                "host=127.0.0.1",     // no store type
+                "postgres:host=db-server,port=5432,db=123,",          // invalid comma at tail
+                "postgres:host=db-server,port=5432,db=test,db=test",  // Duplicated key
+                "postgres:host=db-server:",
+                "postgres:=",
+                "postgres:==",
+                "postgres:=,",
+                "postgres:,,",
+                "postgres:a=b:=",
+                "postgres:a=c:=b",
+                "postgres:a=b:c=d:",
+                "postgres:a=b:c=d:e=f",
+                "postgres:a=%G",
+        };
+        for (String invalidPattern : dsiStringsWithInvalidPattern) {
+            assertThrowsIllegalArgumentExceptionWhenBuildingURI(invalidPattern);
+        }
+    }
+
+    private void assertThrowsIllegalArgumentExceptionWhenBuildingURI(String uriString) {
+        try {
+            DSI.from(uriString);
+            System.err.printf("FAILED TO DETECT INVALID STRING: \"%s\"", uriString);
+            fail();
+        } catch (Exception e) {
+            assertThat(e, instanceOf(IllegalArgumentException.class));
+        }
+    }
+
+    @Test
+    public void cloneDSI_shouldWorkProperly() {
+        // Prepare
+        DSI originalDSI = DSI.from("mysql:host=localhost,port=5432:user=root,password=1234");
+
+        // Process
+        DSI unmodifiedDSI = originalDSI.cloneBuilder().build();
+        DSI modifiedDSI = originalDSI.cloneBuilder()
+                .putProperty("host", "127.0.0.1")
+                .putExtra("password", "he,llo@2020")
+                .build();
+
+        // Validate
+        assertEquals(originalDSI, unmodifiedDSI);
+        assertEquals(originalDSI.toString(), unmodifiedDSI.toString());
+
+        assertEquals("mysql:host=127.0.0.1,port=5432:password=he%2Cllo%402020,user=root", modifiedDSI.toString());
+    }
+}
