4e64e4d9cc5a9c50b6e586687add948b4a5bf998
jiechen <jie.chen@mioying.com>
2020-11-02 15:02:46 +0800

    add security user info api
    Closes #228

--- kun-security/kun-security-common/src/main/java/com/miotech/kun/security/service/BaseSecurityService.java
+++ kun-security/kun-security-common/src/main/java/com/miotech/kun/security/service/BaseSecurityService.java
@@ -1,13 +1,25 @@
 package com.miotech.kun.security.service;
 
+import com.miotech.kun.common.model.RequestResult;
 import com.miotech.kun.security.SecurityContextHolder;
+import com.miotech.kun.security.common.ConfigKey;
 import com.miotech.kun.security.model.bo.UserInfo;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author: Jie Chen
  * @created: 2020/7/1
  */
 public class BaseSecurityService {
+
+    /**
+     * separate from app rest template
+     */
+    RestTemplate restTemplate = new RestTemplate();
 
     public String getCurrentUsername() {
         UserInfo userInfo = getCurrentUser();
@@ -19,5 +31,19 @@ public class BaseSecurityService {
 
     public UserInfo getCurrentUser() {
         return SecurityContextHolder.getUserInfo();
+    }
+
+    public UserInfo getUserById(Long id) {
+        HttpEntity httpEntity = new HttpEntity(SecurityContextHolder.getHttpHeaderInfo());
+        String userInfoUrl = ConfigKey.getSecurityServerUserInfoUrl() + id;
+        ResponseEntity<RequestResult<UserInfo>> userResult = restTemplate.exchange(userInfoUrl,
+                HttpMethod.GET,
+                httpEntity,
+                new ParameterizedTypeReference<RequestResult<UserInfo>>() {
+                });
+        if (!userResult.getStatusCode().is2xxSuccessful()) {
+            return null;
+        }
+        return userResult.getBody().getResult();
     }
 }
