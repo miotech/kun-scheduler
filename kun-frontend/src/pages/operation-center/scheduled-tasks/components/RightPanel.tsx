9917348b6634955c1936b2d17e4cdb1902110198
Junpeng Ouyang <josh.ouyang@mioying.com>
2020-11-02 21:14:58 +0800

    fix(taskrun-ui): change layout to tabs

--- kun-frontend/src/pages/operation-center/scheduled-tasks/components/RightPanel.tsx
+++ kun-frontend/src/pages/operation-center/scheduled-tasks/components/RightPanel.tsx
@@ -1,8 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import styles from '@/pages/operation-center/deployed-task-detail/index.less';
 import { TaskRunDAG } from '@/pages/operation-center/deployed-task-detail/components/TaskRunDAG';
 import { TaskRunLog } from '@/pages/operation-center/deployed-task-detail/components/TaskRunLog';
 import { TaskRun } from '@/definitions/TaskRun.type';
+import { Tabs } from 'antd';
 
 interface OwnProps {
   rightPanelRef?: any;
@@ -20,24 +21,42 @@ export const RightPanel: React.FC<Props> = memo(function RightPanel(props) {
     dagContainerSize,
   } = props;
 
+  const [ currentTab, setCurrentTab ] = useState<string>('logs');
+
+  const handleTabChange = useCallback((activeKey: string) => {
+    setCurrentTab(activeKey);
+  }, []);
+
   return (
     <div
       className={styles.RightPanel}
       ref={rightPanelRef}
     >
-      <div
-        id="taskrun-dag-container"
-        className={styles.DAGContainer}
+      <Tabs
+        type="card"
+        defaultActiveKey="logs"
+        activeKey={currentTab}
+        onChange={handleTabChange}
+        className={styles.RightPanelTabsContainer}
       >
-        <TaskRunDAG
-          taskRun={selectedTaskRun}
-          width={(dagContainerSize.width ?? 0)}
-          height={(dagContainerSize.height ?? 0) / 2}
-        />
-      </div>
-      <TaskRunLog
-        taskRun={selectedTaskRun}
-      />
+        <Tabs.TabPane tab="Logs" key="logs">
+          <TaskRunLog
+            taskRun={selectedTaskRun}
+          />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="DAG" key="dag">
+          <div
+            id="taskrun-dag-container"
+            className={styles.DAGContainer}
+          >
+            <TaskRunDAG
+              taskRun={selectedTaskRun}
+              width={(dagContainerSize.width ?? 0)}
+              height={(dagContainerSize.height ?? 20) - 20}
+            />
+          </div>
+        </Tabs.TabPane>
+      </Tabs>
     </div>
   );
 });
