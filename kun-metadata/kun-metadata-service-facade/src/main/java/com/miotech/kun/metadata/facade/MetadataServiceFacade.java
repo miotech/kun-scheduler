f27318896d4d10d3f923f7d16db69a1fccd58d0c
Josh Ouyang <josh.ouyang@mioying.com>
2020-11-02 12:10:35 +0800

    fix: patch issues on pull request #214 (#227)

--- kun-metadata/kun-metadata-service-facade/src/main/java/com/miotech/kun/metadata/facade/MetadataServiceFacade.java
+++ kun-metadata/kun-metadata-service-facade/src/main/java/com/miotech/kun/metadata/facade/MetadataServiceFacade.java
@@ -1,6 +1,5 @@
 package com.miotech.kun.metadata.facade;
 
-import com.miotech.kun.metadata.core.model.DSI;
 import com.miotech.kun.metadata.core.model.DataStore;
 import com.miotech.kun.metadata.core.model.Dataset;
 
@@ -15,13 +14,5 @@ public interface MetadataServiceFacade {
      * @param datastore key datastore object
      * @return Dataset model object. Returns null if not found by datastore key.
      */
-    @Deprecated
     Dataset getDatasetByDatastore(DataStore datastore);
-
-    /**
-     * Obtain dataset model object (from remote) by given DSI as search key.
-     * @param dataStoreIdentifier the data store identifier that represents target dataset
-     * @return Dataset model object. Returns null if not found by datastore key.
-     */
-    Dataset getDatasetByDSI(DSI dataStoreIdentifier);
 }
