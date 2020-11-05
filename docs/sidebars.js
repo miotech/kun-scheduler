bb2046ac40b6ffa594a1023270ab05c2bc5613f3
Melo <melo@mioying.com>
2020-11-05 10:26:03 +0800

    Enhance Doc Structure Signed-off-by: Melo <melo@mioying.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   'docs': {
-    QuickStart: ['quick-start-zh', 'quick-start-en'],
-    Overview: ['architecture-zh', 'concepts-zh', 'workflow-zh', 'metadata-zh'],
+    QuickStart: ['quick-start-setup-zh', 'quick-start-data-discovery-zh', 'quick-start-data-dashboard-zh'],
+    Overview: ['architecture-zh', 'concepts-zh', 'module-kun-data-discovery-zh', 'module-kun-data-dashboard-zh', 'module-workflow-zh', 'module-kun-security-zh'],
   },
 };
