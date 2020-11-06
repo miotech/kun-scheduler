38b81c216d65baa882e87758b621a3b2e6be2afb
Junpeng Ouyang <josh.ouyang@mioying.com>
2020-11-06 12:08:38 +0800

    fix: styles and i18n

--- kun-frontend/src/pages/operation-center/scheduled-tasks/components/RightPanel.tsx
+++ kun-frontend/src/pages/operation-center/scheduled-tasks/components/RightPanel.tsx
@@ -1,9 +1,12 @@
 import React, { memo, useCallback, useState } from 'react';
-import styles from '@/pages/operation-center/deployed-task-detail/index.less';
+import { Tabs } from 'antd';
+import useI18n from '@/hooks/useI18n';
+import { FileSearchOutlined, ForkOutlined } from '@ant-design/icons';
 import { TaskRunDAG } from '@/pages/operation-center/deployed-task-detail/components/TaskRunDAG';
 import { TaskRunLog } from '@/pages/operation-center/deployed-task-detail/components/TaskRunLog';
 import { TaskRun } from '@/definitions/TaskRun.type';
-import { Tabs } from 'antd';
+
+import styles from '@/pages/operation-center/deployed-task-detail/index.less';
 
 interface OwnProps {
   rightPanelRef?: any;
@@ -20,6 +23,8 @@ export const RightPanel: React.FC<Props> = memo(function RightPanel(props) {
     selectedTaskRun,
     dagContainerSize,
   } = props;
+
+  const t = useI18n();
 
   const [ currentTab, setCurrentTab ] = useState<string>('logs');
 
@@ -39,12 +44,20 @@ export const RightPanel: React.FC<Props> = memo(function RightPanel(props) {
         onChange={handleTabChange}
         className={styles.RightPanelTabsContainer}
       >
-        <Tabs.TabPane tab="Logs" key="logs">
+        <Tabs.TabPane tab={<span>
+          <FileSearchOutlined />
+          <span>{t('scheduledTasks.logs')}</span>
+        </span>} key="logs">
           <TaskRunLog
             taskRun={selectedTaskRun}
           />
         </Tabs.TabPane>
-        <Tabs.TabPane tab="DAG" key="dag">
+        <Tabs.TabPane tab={
+          <span>
+            <ForkOutlined />
+            <span>{t('scheduledTasks.DAG')}</span>
+          </span>
+        } key="dag">
           <div
             id="taskrun-dag-container"
             className={styles.DAGContainer}
