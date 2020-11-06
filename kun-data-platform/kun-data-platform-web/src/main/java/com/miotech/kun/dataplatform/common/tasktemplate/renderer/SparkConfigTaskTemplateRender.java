7c082abd986ecc53f573fe6310132c379d52b0af
zhangxinwei <xinwei.zhang@mioying.com>
2020-11-06 11:46:48 +0800

    [Fix]An error occurred when the TaskDefination config was passed to the Task

--- kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/common/tasktemplate/renderer/SparkConfigTaskTemplateRender.java
+++ kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/common/tasktemplate/renderer/SparkConfigTaskTemplateRender.java
@@ -7,7 +7,6 @@ import com.miotech.kun.workflow.client.model.ConfigKey;
 import com.miotech.kun.workflow.utils.JSONUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -75,9 +74,11 @@ public class SparkConfigTaskTemplateRender extends TaskTemplateRenderer {
                     extraJavaOptions.append(String.format(" -D%s.%s=${dataplatform.datasource.%s}", KUN_DATA_PLATFORM_DATASOURCE_PREFIX, configMap.get(key), configMap.get(key)));
                 }
             }
-            String encodedString = Base64.getEncoder().encodeToString(JSONUtils.toJsonString(extraConfig).getBytes());
+            /*if (!extraConfig.isEmpty()) {
+                String encodedString = Base64.getEncoder().encodeToString(JSONUtils.toJsonString(extraConfig).getBytes());
+                extraJavaOptions.append(String.format(" -D%s=%s", KUN_DATA_PLATFORM_CONFIG_PREFIX, encodedString));
+            }*/
 
-            extraJavaOptions.append(String.format(" -D%s=%s", KUN_DATA_PLATFORM_CONFIG_PREFIX, encodedString));
             sparkConfig.put(SPARK_JAVA_OPTIONS_KEY, extraJavaOptions.toString());
             config.put(SPARK_CONFIG_KEY, JSONUtils.toJsonString(sparkConfig));
         } else {
