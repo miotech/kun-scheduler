6cc64c1406de56ce1512a3ae7c7a4e914535465c
guofan1991 <472637781@qq.com>
2020-11-12 19:39:40 +0800

    fix delete task (#170)
    * fix delete task
    
    * offline task
    
    * refactor code, add test
    
    Co-authored-by: aijia <aijia.guo@mioying.com>

--- kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/common/taskdefinition/service/TaskDefinitionService.java
+++ kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/common/taskdefinition/service/TaskDefinitionService.java
@@ -4,12 +4,18 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.miotech.kun.dataplatform.common.commit.service.TaskCommitService;
 import com.miotech.kun.dataplatform.common.datastore.service.DatasetService;
+import com.miotech.kun.dataplatform.common.deploy.service.DeployService;
+import com.miotech.kun.dataplatform.common.deploy.service.DeployedTaskService;
+import com.miotech.kun.dataplatform.common.deploy.vo.DeployRequest;
 import com.miotech.kun.dataplatform.common.taskdefinition.dao.TaskDefinitionDao;
 import com.miotech.kun.dataplatform.common.taskdefinition.dao.TaskTryDao;
 import com.miotech.kun.dataplatform.common.taskdefinition.vo.*;
 import com.miotech.kun.dataplatform.common.tasktemplate.service.TaskTemplateService;
 import com.miotech.kun.dataplatform.common.utils.DataPlatformIdGenerator;
 import com.miotech.kun.dataplatform.common.utils.TagUtils;
+import com.miotech.kun.dataplatform.model.commit.TaskCommit;
+import com.miotech.kun.dataplatform.model.deploy.Deploy;
+import com.miotech.kun.dataplatform.model.deploy.DeployedTask;
 import com.miotech.kun.dataplatform.model.taskdefinition.*;
 import com.miotech.kun.dataplatform.model.tasktemplate.ParameterDefinition;
 import com.miotech.kun.dataplatform.model.tasktemplate.TaskTemplate;
@@ -51,6 +57,12 @@ public class TaskDefinitionService extends BaseSecurityService {
 
     @Autowired
     private DatasetService datasetService;
+
+    @Autowired
+    private DeployedTaskService deployedTaskService;
+
+    @Autowired
+    private DeployService deployService;
 
     @Autowired
     @Lazy
@@ -212,7 +224,21 @@ public class TaskDefinitionService extends BaseSecurityService {
             throw new IllegalArgumentException(String.format("Task definition not found: \"%s\"", taskDefId));
         }
         taskDefinitionDao.archive(taskDefId);
-        taskCommitService.commit(taskDefId, "OFFLINE");
+
+        //offline deployed task
+        TaskCommit commit = taskCommitService.commit(taskDefId, "OFFLINE");
+        try{
+            DeployedTask deployedTask = deployedTaskService.find(taskDefId);
+            List<Long> commitIds = new ArrayList<>();
+            commitIds.add(commit.getId());
+            DeployRequest request = new DeployRequest();
+            request.setCommitIds(commitIds);
+            Deploy deploy = deployService.create(request);
+            deployService.publish(deploy.getId());
+        }catch (IllegalArgumentException e){
+            log.debug("task definition \"{}\" not deployed yet, no need to offline", taskDefId);
+        }
+
     }
 
     public List<Long> resolveUpstreamTaskDefIds(TaskPayload taskPayload) {
