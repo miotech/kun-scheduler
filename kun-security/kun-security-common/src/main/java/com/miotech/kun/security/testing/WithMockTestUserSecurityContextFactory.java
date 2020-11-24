3fe052ad501c523dfc819bca3cb80468dae639d2
jiechen <jie.chen@mioying.com>
2020-11-24 10:52:17 +0800

    enhance security permission logic
    Closes #309

--- kun-security/kun-security-common/src/main/java/com/miotech/kun/security/testing/WithMockTestUserSecurityContextFactory.java
+++ kun-security/kun-security-common/src/main/java/com/miotech/kun/security/testing/WithMockTestUserSecurityContextFactory.java
@@ -1,5 +1,6 @@
 package com.miotech.kun.security.testing;
 
+import com.miotech.kun.security.common.Permission;
 import com.miotech.kun.security.model.bo.UserInfo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.context.SecurityContext;
@@ -17,8 +18,9 @@ public class WithMockTestUserSecurityContextFactory
     public SecurityContext createSecurityContext(WithMockTestUser userInfo) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-        Set<String> perms = Arrays.stream(userInfo.permissions())
+        Set<Permission> perms = Arrays.stream(userInfo.permissions())
                 .filter(StringUtils::isNoneBlank)
+                .map(Permission::valueOf)
                 .collect(Collectors.toSet());
 
         UserInfo principal = new UserInfo();
