57b26eea17d1319ed51dba6dcdb3120265750c79
seaway-z <70014892+seaway-z@users.noreply.github.com>
2020-12-21 10:29:29 +0800

    feature: set job name (#342)

--- kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/common/tasktemplate/renderer/DefaultTaskTemplateRenderer.java
+++ kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/common/tasktemplate/renderer/DefaultTaskTemplateRenderer.java
@@ -1,6 +1,7 @@
 package com.miotech.kun.dataplatform.common.tasktemplate.renderer;
 
 import com.miotech.kun.dataplatform.model.taskdefinition.TaskConfig;
+import com.miotech.kun.dataplatform.model.taskdefinition.TaskDefinition;
 import com.miotech.kun.dataplatform.model.tasktemplate.TaskTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +11,8 @@ import java.util.Map;
 public class DefaultTaskTemplateRenderer extends TaskTemplateRenderer {
 
     @Override
-    public TaskConfig render(Map<String, Object> taskConfig, TaskTemplate taskTemplate) {
-        Map<String, Object> configMap = buildTaskConfig(taskConfig, taskTemplate);
+    public TaskConfig render(Map<String, Object> taskConfig, TaskTemplate taskTemplate, TaskDefinition taskDefinition) {
+        Map<String, Object> configMap = buildTaskConfig(taskConfig, taskTemplate, taskDefinition);
 
         return TaskConfig.newBuilder()
                 .withParams(configMap)
