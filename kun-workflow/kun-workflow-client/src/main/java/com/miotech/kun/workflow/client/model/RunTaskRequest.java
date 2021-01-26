536aee9eafe02745ea44f848ad368055d0a7ca56
Junpeng Ouyang <josh.ouyang@mioying.com>
2021-01-26 17:55:49 +0800

    test(data-backfill): add test cases for backfill service

--- kun-workflow/kun-workflow-client/src/main/java/com/miotech/kun/workflow/client/model/RunTaskRequest.java
+++ kun-workflow/kun-workflow-client/src/main/java/com/miotech/kun/workflow/client/model/RunTaskRequest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Maps;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class RunTaskRequest {
@@ -20,5 +21,18 @@ public class RunTaskRequest {
                         x.getValue())
                 )
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RunTaskRequest that = (RunTaskRequest) o;
+        return Objects.equals(taskConfigs, that.taskConfigs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(taskConfigs);
     }
 }
