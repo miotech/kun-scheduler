bf232322e6c4c2ce818b23a05ca1b54727f717f1
Junpeng Ouyang <josh.ouyang@mioying.com>
2021-02-26 17:12:23 +0800

    chore(task-notification): define notification data structure & add db migration scripts

--- kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/model/notify/TaskStatusNotifyTrigger.java
+++ kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/model/notify/TaskStatusNotifyTrigger.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum TaskStatusNotifyTrigger {
     SYSTEM_DEFAULT("SYSTEM_DEFAULT"),
-    SUCCESS("SUCCESS"),
-    FAILED("FAILED"),
-    FINISHED("FINISHED"),
+    ON_SUCCESS("ON_SUCCESS"),
+    ON_FAIL("ON_FAIL"),
+    ON_FINISH("ON_FINISH"),
     NEVER("NEVER");
 
     private final String typeName;
@@ -18,5 +18,22 @@ public enum TaskStatusNotifyTrigger {
 
     TaskStatusNotifyTrigger(String typeName) {
         this.typeName = typeName;
+    }
+
+    public static TaskStatusNotifyTrigger from(String value) {
+        switch (value) {
+            case "SYSTEM_DEFAULT":
+                return SYSTEM_DEFAULT;
+            case "ON_SUCCESS":
+                return ON_SUCCESS;
+            case "ON_FAIL":
+                return ON_FAIL;
+            case "ON_FINISH":
+                return ON_FINISH;
+            case "NEVER":
+                return NEVER;
+            default:
+                throw new IllegalArgumentException(String.format("Unknown task status notifier trigger type: \"%s\"", value));
+        }
     }
 }
---
+++ kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/model/taskdefinition/TaskDefNotifyConfig.java
@@ -0,0 +1,31 @@
+package com.miotech.kun.dataplatform.model.taskdefinition;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.miotech.kun.dataplatform.model.notify.TaskStatusNotifyTrigger;
+import com.miotech.kun.dataplatform.notify.userconfig.NotifierUserConfig;
+
+import java.util.List;
+
+public class TaskDefNotifyConfig {
+    @JsonProperty("notifyWhen")
+    private final TaskStatusNotifyTrigger notifyWhen;
+
+    @JsonProperty("notifierConfig")
+    private final List<NotifierUserConfig> notifierUserConfigList;
+
+    public TaskDefNotifyConfig(
+            @JsonProperty("notifyWhen") TaskStatusNotifyTrigger notifyWhen,
+            @JsonProperty("notifierConfig") List<NotifierUserConfig> notifierUserConfigList
+    ) {
+        this.notifyWhen = notifyWhen;
+        this.notifierUserConfigList = notifierUserConfigList;
+    }
+
+    public TaskStatusNotifyTrigger getNotifyWhen() {
+        return notifyWhen;
+    }
+
+    public List<NotifierUserConfig> getNotifierUserConfigList() {
+        return notifierUserConfigList;
+    }
+}
