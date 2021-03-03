af4ece0126a71b03ef5b989b345c36601594c2c0
shikimoe <w_yide@163.com>
2021-03-03 16:55:19 +0800

    fix

--- kun-workflow/kun-workflow-common/src/main/resources/sql/V13__alter_dependency_status.sql
+++ kun-workflow/kun-workflow-common/src/main/resources/sql/V13__alter_dependency_status.sql
@@ -1,1 +1,3 @@
-ALTER TABLE ONLY kun_wf_task_run_relations ALTER COLUMN dependency_status SET DEFAULT 'CREATED';
+ALTER TABLE  kun_wf_task_run_relations DROP COLUMN dependency_status
+
+ALTER TABLE kun_wf_task_run_relations ADD COLUMN dependency_status VARCHAR(64) NOT NULL DEFAULT 'CREATED';
