217877f8adf2a8f61da7e18b636e4b9f42b49c70
seaway-z <70014892+seaway-z@users.noreply.github.com>
2021-03-15 09:59:46 +0800

    Feature: Add cache (#440)

--- kun-data-dashboard/src/main/java/com/miotech/kun/datadashboard/service/MetadataService.java
+++ kun-data-dashboard/src/main/java/com/miotech/kun/datadashboard/service/MetadataService.java
@@ -1,5 +1,8 @@
 package com.miotech.kun.datadashboard.service;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.miotech.kun.commons.utils.ExceptionUtils;
 import com.miotech.kun.datadashboard.model.bo.ColumnMetricsRequest;
 import com.miotech.kun.datadashboard.model.bo.RowCountChangeRequest;
 import com.miotech.kun.datadashboard.model.bo.TestCasesRequest;
@@ -12,12 +15,23 @@ import com.miotech.kun.datadashboard.persistence.MetadataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author: Jie Chen
  * @created: 2020/9/15
  */
 @Service
 public class MetadataService {
+
+    private final static String ROW_COUNT_CHANGES_FORMAT = "row_count_change:page_number=%d:page_size=%d";
+    private final static String COLUMN_METRICS_FORMAT = "column_metrics:page_number=%d:page_size=%d";
+    private final static String METADATA_METRICS = "metadata_metrics";
+    private final static Cache<String, Object> localCache = CacheBuilder.newBuilder()
+            .maximumSize(10)
+            .expireAfterWrite(1, TimeUnit.DAYS)
+            .build();
 
     @Autowired
     DataQualityRepository dataQualityRepository;
@@ -26,17 +40,29 @@ public class MetadataService {
     MetadataRepository metadataRepository;
 
     public MetadataMetrics getMetadataMetrics() {
-        MetadataMetrics metrics = new MetadataMetrics();
-        metrics.setTotalCaseCount(dataQualityRepository.getTotalCaseCount());
-        metrics.setTotalDatasetCount(metadataRepository.getTotalDatasetCount());
-        metrics.setDataQualityCoveredCount(dataQualityRepository.getCoveredDatasetCount());
-        metrics.setDataQualityLongExistingFailedCount(dataQualityRepository.getLongExistingCount());
-        metrics.setDataQualityPassCount(dataQualityRepository.getSuccessCount());
-        return metrics;
+        try {
+            return (MetadataMetrics) localCache.get(METADATA_METRICS, () -> {
+                MetadataMetrics metrics = new MetadataMetrics();
+                metrics.setTotalCaseCount(dataQualityRepository.getTotalCaseCount());
+                metrics.setTotalDatasetCount(metadataRepository.getTotalDatasetCount());
+                metrics.setDataQualityCoveredCount(dataQualityRepository.getCoveredDatasetCount());
+                metrics.setDataQualityLongExistingFailedCount(dataQualityRepository.getLongExistingCount());
+                metrics.setDataQualityPassCount(dataQualityRepository.getSuccessCount());
+
+                return metrics;
+            });
+        } catch (ExecutionException e) {
+            throw ExceptionUtils.wrapIfChecked(e);
+        }
     }
 
     public DatasetRowCountChanges getRowCountChange(RowCountChangeRequest rowCountChangeRequest) {
-        return metadataRepository.getRowCountChange(rowCountChangeRequest);
+        String key = String.format(ROW_COUNT_CHANGES_FORMAT, rowCountChangeRequest.getPageNumber(), rowCountChangeRequest.getPageSize());
+        try {
+            return (DatasetRowCountChanges) localCache.get(key, () -> metadataRepository.getRowCountChange(rowCountChangeRequest));
+        } catch (ExecutionException e) {
+            throw ExceptionUtils.wrapIfChecked(e);
+        }
     }
 
     public DataQualityCases getTestCases(TestCasesRequest testCasesRequest) {
@@ -44,6 +70,11 @@ public class MetadataService {
     }
 
     public ColumnMetricsList getColumnMetricsList(ColumnMetricsRequest columnMetricsRequest) {
-        return metadataRepository.getColumnMetricsList(columnMetricsRequest);
+        String key = String.format(COLUMN_METRICS_FORMAT, columnMetricsRequest.getPageNumber(), columnMetricsRequest.getPageSize());
+        try {
+            return (ColumnMetricsList) localCache.get(key, () -> metadataRepository.getColumnMetricsList(columnMetricsRequest));
+        } catch (ExecutionException e) {
+            throw ExceptionUtils.wrapIfChecked(e);
+        }
     }
 }
