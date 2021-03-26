3b1c83e8d396550c2bbe609e51cf1948631c9299
shikimoe <w_yide@163.com>
2021-03-26 11:05:36 +0800

    fix

--- kun-workflow/kun-workflow-executor/src/test/java/com/miotech/kun/workflow/executor/kubernetes/PodEventMonitorTest.java
+++ kun-workflow/kun-workflow-executor/src/test/java/com/miotech/kun/workflow/executor/kubernetes/PodEventMonitorTest.java
@@ -1,5 +1,18 @@
 package com.miotech.kun.workflow.executor.kubernetes;
 
-public class PodEventMonitorTest {
+import com.miotech.kun.commons.testing.GuiceTestBase;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.junit.Test;
 
+public class PodEventMonitorTest extends GuiceTestBase {
+
+    @Override
+    protected void configuration() {
+        KubernetesClient client = mock(KubernetesClient.class);
+    }
+
+    @Test
+    public void pollingEventTest(){
+
+    }
 }
