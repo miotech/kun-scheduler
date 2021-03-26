e88b04e690e1b0f7cc925326284873bf78576299
shikimoe <w_yide@163.com>
2021-03-26 10:59:41 +0800

    pod event monitor

---
+++ kun-workflow/kun-workflow-executor/src/main/java/com/miotech/kun/workflow/executor/kubernetes/KubernetesConstants.java
@@ -0,0 +1,7 @@
+package com.miotech.kun.workflow.executor.kubernetes;
+
+//管理kubernetes pod的常量，如lables等
+public class KubernetesConstants {
+    public static final String KUN_TASK_ATTEMPT_ID = "kun-task-attempt-id";
+    public static final String KUN_WORKFLOW = "kun-workflow";
+}
