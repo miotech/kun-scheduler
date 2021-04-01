69d4bc6dbb315d503d8764fe9d6a51e3fa1090bc
shikimoe <w_yide@163.com>
2021-04-01 10:25:33 +0800

    kubernetes operator launcher

--- kun-workflow/kun-workflow-executor/src/main/java/com/miotech/kun/workflow/executor/kubernetes/KubernetesConstants.java
+++ kun-workflow/kun-workflow-executor/src/main/java/com/miotech/kun/workflow/executor/kubernetes/KubernetesConstants.java
@@ -2,6 +2,6 @@ package com.miotech.kun.workflow.executor.kubernetes;
 
 //管理kubernetes pod的常量，如lables等
 public class KubernetesConstants {
-    public static final String KUN_TASK_ATTEMPT_ID = "kun-task-attempt-id";
-    public static final String KUN_WORKFLOW = "kun-workflow";
+    public static final String KUN_TASK_ATTEMPT_ID = "kuntaskattemptid";
+    public static final String KUN_WORKFLOW = "kunworkflow";
 }
