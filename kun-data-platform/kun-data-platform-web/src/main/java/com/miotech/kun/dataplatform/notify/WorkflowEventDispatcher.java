657439144a01da27e5470b84922640ffff6e2618
Junpeng Ouyang <josh.ouyang@mioying.com>
2021-04-12 15:56:23 +0800

    fix(task-notification): add more logs & handle unregistered workflow tasks

--- kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/notify/WorkflowEventDispatcher.java
+++ kun-data-platform/kun-data-platform-web/src/main/java/com/miotech/kun/dataplatform/notify/WorkflowEventDispatcher.java
@@ -13,15 +13,13 @@ import com.miotech.kun.dataplatform.notify.userconfig.NotifierUserConfig;
 import com.miotech.kun.dataplatform.notify.userconfig.WeComNotifierUserConfig;
 import com.miotech.kun.workflow.core.event.TaskAttemptStatusChangeEvent;
 import com.miotech.kun.workflow.core.publish.EventSubscriber;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
-
+@Slf4j
 public class WorkflowEventDispatcher {
     @Autowired
     private EventSubscriber workflowEventSubscriber;
@@ -54,27 +52,40 @@ public class WorkflowEventDispatcher {
     private void handleTaskAttemptStatusChangeEvent(TaskAttemptStatusChangeEvent statusChangeEvent) {
         // 1. Read task id from event payload
         Long workflowTaskId = statusChangeEvent.getTaskId();
+        log.debug("Receiving `TaskAttemptStatusChangeEvent` with workflow task id = {}", workflowTaskId);
         // 2. Is there any task notify config relates to this task id?
         Optional<TaskNotifyConfig> taskNotifyConfigOptional = taskNotifyConfigService.fetchTaskNotifyConfigByWorkflowTaskId(workflowTaskId);
 
+        TaskNotifyConfig notifyConfig;
         if (taskNotifyConfigOptional.isPresent()) {
+            log.debug("`taskNotifyConfig` exists, value = {}", taskNotifyConfigOptional.get());
             // 3. If there is, read the configuration and notify by notifiers
-            TaskNotifyConfig notifyConfig = taskNotifyConfigOptional.get();
+            notifyConfig = taskNotifyConfigOptional.get();
+        } else {
+            // Use system default notify configuration
+            log.debug("Cannot find `taskNotifyConfig` for workflow task id = {}. Using system default config.", workflowTaskId);
+            notifyConfig = TaskNotifyConfig.newBuilder()
+                    .withWorkflowTaskId(workflowTaskId)
+                    .withNotifierConfigs(Collections.emptyList())
+                    .withTriggerType(TaskStatusNotifyTrigger.SYSTEM_DEFAULT)
+                    .build();
+        }
 
-            // 4. Does this change event match configuration trigger type?
-            boolean matchFlag;
-            if (Objects.equals(notifyConfig.getTriggerType(), TaskStatusNotifyTrigger.SYSTEM_DEFAULT)) {
-                matchFlag = systemDefaultNotifierConfig.getSystemDefaultTriggerType().matches(statusChangeEvent.getToStatus());
-            } else {
-                matchFlag = notifyConfig.test(statusChangeEvent);
-            }
+        // 4. Does this change event match configuration trigger type?
+        boolean matchFlag;
+        if (Objects.equals(notifyConfig.getTriggerType(), TaskStatusNotifyTrigger.SYSTEM_DEFAULT)) {
+            matchFlag = systemDefaultNotifierConfig.getSystemDefaultTriggerType().matches(statusChangeEvent.getToStatus());
+        } else {
+            matchFlag = notifyConfig.test(statusChangeEvent);
+        }
+        log.debug("`matchFlag` of current event = {}.", matchFlag);
 
-            // 5. If matches, construct notifiers
-            if (matchFlag) {
-                List<MessageNotifier> notifiers = constructNotifiersFromNotifyConfig(notifyConfig);
-                // 6. Notify by each notifier
-                notifiers.forEach(notifier -> notifier.notify(statusChangeEvent));
-            }
+        // 5. If matches, construct notifiers
+        if (matchFlag) {
+            List<MessageNotifier> notifiers = constructNotifiersFromNotifyConfig(notifyConfig);
+            log.debug("`notifiers` of current event = {}.", notifiers);
+            // 6. Notify by each notifier
+            notifiers.forEach(notifier -> notifier.notify(statusChangeEvent));
         }
     }
 
