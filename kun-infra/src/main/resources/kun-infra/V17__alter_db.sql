107d9fd4a1b16f4098e3335d3650537ad2626626
shikimoe <w_yide@163.com>
2021-05-26 10:42:34 +0800

    merge flyway

---
+++ kun-infra/src/main/resources/kun-infra/V17__alter_db.sql
@@ -0,0 +1,2 @@
+ALTER TABLE kun_mt_dataset_field ADD COLUMN IF NOT EXISTS "is_primary_key" BOOLEAN DEFAULT FALSE;
+ALTER TABLE kun_mt_dataset_field ADD COLUMN IF NOT EXISTS "is_nullable" BOOLEAN DEFAULT TRUE;
