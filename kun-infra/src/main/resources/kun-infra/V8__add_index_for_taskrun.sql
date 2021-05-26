742186050a33764daa22897d2b67d57be0725e0e
shikimoe <w_yide@163.com>
2021-05-26 15:13:48 +0800

    fix none schedule task
    test case

--- kun-infra/src/main/resources/kun-infra/V8__add_index_for_taskrun.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS kun_wf_task_run_created_at_index
-    on kun_wf_task_run (created_at);
--- kun-metadata/kun-metadata-common/src/main/resources/sql/V8__alter_db.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE kun_mt_dataset_stats ADD COLUMN "total_byte_size" BIGINT;
