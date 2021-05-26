1aa4ff6c769d18644c043ef10321368b030741e8
shikimoe <w_yide@163.com>
2021-05-26 15:13:48 +0800

    fix none schedule task
    test case

--- kun-infra/src/main/resources/kun-infra/V10__add_queue_name.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE kun_wf_task ADD queue_name VARCHAR(64)  DEFAULT 'default';
-ALTER TABLE kun_wf_task_run ADD queue_name VARCHAR(64)  DEFAULT 'default';
-ALTER TABLE kun_wf_task_attempt ADD queue_name VARCHAR(64)  DEFAULT 'default';
