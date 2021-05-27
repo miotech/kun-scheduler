2905a120796008f7d96c9e7ea1c5005c720ec855
Ye Ding <dygvirus@gmail.com>
2021-05-27 18:14:59 +0800

    Docker compose/ye.ding (#515)
    * Make docker-compose work
    
    * Use shanghai as default timezone
    
    * Remove unnecessary tasks
    
    * add spring profile in environment variables

--- kun-data-discovery/docker-entrypoint.sh
+++ kun-data-discovery/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-dockerize -wait http://${METADATA_HOST}:${METADATA_PORT}/health -wait-retry-interval 3s -timeout 60s
+dockerize -wait http://${METADATA_HOST}:${METADATA_PORT}/health -wait-retry-interval 3s -timeout 180s
 java ${JVM_OPTS} -jar /server/target/kun-data-discovery.jar
--- kun-security/docker-entrypoint.sh
+++ kun-security/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-dockerize -wait tcp://${DB_PG_IP}:${DB_PG_PORT} -wait-retry-interval 1s -timeout 60s
-dockerize -wait tcp://${DB_NEO4J_IP}:7687 -wait-retry-interval 1s -timeout 60s
+dockerize -wait tcp://${DB_PG_IP}:${DB_PG_PORT} -wait-retry-interval 1s -timeout 180s
+dockerize -wait tcp://${DB_NEO4J_IP}:${DB_NEO4J_PORT} -wait-retry-interval 1s -timeout 180s
 java ${JVM_OPTS} -jar /server/target/kun-security-server.jar
