4dcfcd741a7f5cec367981ebbd8130df6bf7b726
dyng <dygvirus@gmail.com>
2021-06-03 16:44:04 +0800

    Fix docker-compose and others

--- kun-app/docker-entrypoint.sh
+++ kun-app/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 dockerize -wait http://${KUN_INFRA_HOST}:${KUN_INFRA_PORT}/health -wait-retry-interval 3s -timeout 180s
-java ${JVM_OPTS} -jar /server/target/kun-data-platform-web.jar
+java ${JVM_OPTS} -jar /server/target/kun-app.jar
