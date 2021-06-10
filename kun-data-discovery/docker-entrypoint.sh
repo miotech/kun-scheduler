cb3e5ad41018bd88fd642adb544cf8c99c543245
dyng <dygvirus@gmail.com>
2021-06-10 14:36:45 +0800

    remove unnecessary docker files

--- kun-data-discovery/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-dockerize -wait http://${KUN_INFRA_HOST}:${KUN_INFRA_PORT}/health -wait-retry-interval 3s -timeout 180s
-java ${JVM_OPTS} -jar /server/target/kun-data-discovery.jar
