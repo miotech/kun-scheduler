8c5ab54e0f9a41bfd70d754017ac57d6571e2b4e
Junpeng Ouyang <josh.ouyang@mioying.com>
2021-06-15 19:24:19 +0800

    feat(ui): display SQL query result table instead of log for "SparkSQL" task template

--- kun-frontend/src/components/QueryResultTable/QueryResultTable.component.tsx
+++ kun-frontend/src/components/QueryResultTable/QueryResultTable.component.tsx
@@ -5,13 +5,9 @@ import { SheetRenderer } from '@/components/QueryResultTable/QueryResultTableShe
 import { SQLQueryRow } from '@/definitions/QueryResult.type';
 import 'react-datasheet/lib/react-datasheet.css';
 import './QueryResultTable.less';
-import { Pagination } from 'antd';
 
 interface OwnProps {
   columnNames?: string[];
-  pageNum?: number;
-  pageSize?: number;
-  total?: number;
   data?: SQLQueryRow[];
 }
 
@@ -20,9 +16,6 @@ type Props = OwnProps;
 export const QueryResultTable: React.FC<Props> = memo(function QueryResultTable(props) {
   const {
     columnNames = [],
-    pageNum = 1,
-    pageSize = 10,
-    total = 0,
     data: propsData = [],
   } = props;
 
@@ -43,14 +36,17 @@ export const QueryResultTable: React.FC<Props> = memo(function QueryResultTable(
       return <span key={`cell-${i}-${j}`}>{cell.value}</span>;
     }
     // else
-    return <span key={`cell-${i}-${j}-null`} className="cell-null">(null)</span>;
+    return <span key={`cell-${i}-${j}-null`} className="cell-null" />;
+  }, []);
+
+  const dataRenderer = useCallback(function dataRenderer(cell) {
+    return cell.value;
   }, []);
 
   const renderSheet = useCallback((otherProps: any) => {
     return (
       <SheetRenderer
         columns={(columnNames.map(colName => ({ label: colName })))}
-        onColumnDrop={() => {}}
         {...otherProps}
       />
     );
@@ -63,11 +59,10 @@ export const QueryResultTable: React.FC<Props> = memo(function QueryResultTable(
         data={data}
         // @ts-ignore
         valueRenderer={valueRenderer}
+        dataRenderer={dataRenderer}
         sheetRenderer={renderSheet}
+        overflow="clip"
       />
-      <div className="query-result-table__pagination-wrapper">
-        <Pagination current={pageNum} pageSize={pageSize} total={total} size="small" />
-      </div>
     </div>
   );
 });
