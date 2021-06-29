fe78639956c8066d6995915466c249aa44444a98
zhangxinwei <xinwei.zhang@mioying.com>
2021-06-29 16:04:24 +0800

    SQL suggest

--- kun-data-discovery/src/main/java/com/miotech/kun/datadiscovery/service/MetadataService.java
+++ kun-data-discovery/src/main/java/com/miotech/kun/datadiscovery/service/MetadataService.java
@@ -3,14 +3,19 @@ package com.miotech.kun.datadiscovery.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.miotech.kun.commons.utils.ExceptionUtils;
-import com.miotech.kun.commons.utils.StringUtils;
 import com.miotech.kun.datadiscovery.model.vo.PullProcessVO;
+import com.miotech.kun.metadata.core.model.DatasetColumnSuggestRequest;
+import com.miotech.kun.metadata.core.model.DatasetColumnSuggestResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -66,5 +71,26 @@ public class MetadataService {
         String fullUrl = url + "/datasets/{datasetId}/_pull/latest";
         log.info("Request url : " + fullUrl);
         return Optional.ofNullable(restTemplate.getForObject(fullUrl, PullProcessVO.class, datasetId));
+    }
+
+    public List<String> suggestDatabase(String prefix) {
+        String suggestDatabaseUrl = url + "/dataset/database/_suggest";
+        if (StringUtils.isNotBlank(prefix)) {
+            suggestDatabaseUrl = suggestDatabaseUrl + "?prefix=" + prefix;
+        }
+        return Arrays.asList(restTemplate.getForEntity(suggestDatabaseUrl, String[].class).getBody());
+    }
+
+    public List<String> suggestTable(String databaseName, String prefix) {
+        String suggestTableUrl = url + "/dataset/table/_suggest?databaseName=" + databaseName;
+        if (StringUtils.isNotBlank(prefix)) {
+            suggestTableUrl = suggestTableUrl + "&prefix=" + prefix;
+        }
+        return Arrays.asList(restTemplate.getForEntity(suggestTableUrl, String[].class).getBody());
+    }
+
+    public List<DatasetColumnSuggestResponse> suggestColumn(List<DatasetColumnSuggestRequest> columnSuggestRequests) {
+        String suggestColumnUrl = url + "/dataset/column/_suggest";
+        return Arrays.asList(restTemplate.exchange(suggestColumnUrl, HttpMethod.POST, new HttpEntity<>(columnSuggestRequests), DatasetColumnSuggestResponse[].class).getBody());
     }
 }
---
+++ kun-metadata/kun-metadata-web/src/main/java/com/miotech/kun/metadata/web/controller/DatasetController.java
@@ -0,0 +1,67 @@
+package com.miotech.kun.metadata.web.controller;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.miotech.kun.commons.web.annotation.QueryParameter;
+import com.miotech.kun.commons.web.annotation.RequestBody;
+import com.miotech.kun.commons.web.annotation.RouteMapping;
+import com.miotech.kun.metadata.common.service.MetadataDatasetService;
+import com.miotech.kun.metadata.core.model.DatasetColumnSuggestRequest;
+import com.miotech.kun.metadata.core.model.DatasetColumnSuggestResponse;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@Singleton
+public class DatasetController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatasetController.class);
+
+    @Inject
+    private MetadataDatasetService datasetService;
+
+    @RouteMapping(url = "/dataset/database/_suggest", method = "GET")
+    public List<String> suggestDatabase(@QueryParameter String prefix) {
+        logger.debug("Suggest database, prefix: {}", prefix);
+        return datasetService.suggestDatabase(prefix);
+    }
+
+    @RouteMapping(url = "/dataset/table/_suggest", method = "GET")
+    public List<String> suggestTable(@QueryParameter(required = true) String databaseName, @QueryParameter String prefix) {
+        logger.debug("Suggest table, databaseName: {}, prefix: {}", databaseName, prefix);
+        return datasetService.suggestTable(databaseName, prefix);
+    }
+
+    @RouteMapping(url = "/dataset/column/_suggest", method = "POST")
+    public List<DatasetColumnSuggestResponse> suggestColumn(@RequestBody List<DatasetColumnSuggestRequest> columnSuggestRequests) {
+        logger.debug("Suggest column, columnSuggestRequests: {}", columnSuggestRequests);
+        String checkParamResult = check(columnSuggestRequests);
+        if (StringUtils.isNotBlank(checkParamResult)) {
+            throw new IllegalArgumentException(checkParamResult);
+        }
+
+        return datasetService.suggestColumn(columnSuggestRequests);
+    }
+
+    private String check(List<DatasetColumnSuggestRequest> columnSuggestRequests) {
+        if (CollectionUtils.isEmpty(columnSuggestRequests)) {
+            return "requests should not be empty";
+        }
+
+        for (DatasetColumnSuggestRequest columnSuggestRequest : columnSuggestRequests) {
+            if (StringUtils.isBlank(columnSuggestRequest.getDatabaseName())) {
+                return "databaseName should not be empty";
+            }
+
+            if (StringUtils.isBlank(columnSuggestRequest.getTableName())) {
+                return "tableName should not be empty";
+            }
+        }
+
+        return StringUtils.EMPTY;
+    }
+
+}
