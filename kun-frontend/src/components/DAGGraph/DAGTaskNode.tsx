e6cfd98079905f8c2e130b207aa4d2d1ba58f5c6
王晨 <wangchen@mioying.com>
2021-10-18 11:19:00 +0800

    ER: fix KUN-194

--- kun-frontend/src/components/DAGGraph/DAGTaskNode.tsx
+++ kun-frontend/src/components/DAGGraph/DAGTaskNode.tsx
@@ -71,6 +71,7 @@ function renderStatus(status: RunStatusEnum) {
     case 'ABORTED':
     case 'ABORTING':
     case 'FAILED':
+    case 'CHECK_FAILED':
       return <Badge status="error" text={status} />;
     case 'SUCCESS':
       return <Badge status="success" text={status} />;
