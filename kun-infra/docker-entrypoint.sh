618afcd658bdc48f605c20e175b940b7d9a52a66
shikimoe <w_yide@163.com>
2021-10-19 09:48:08 +0800

    fix docker file

--- kun-infra/docker-entrypoint.sh
+++ kun-infra/docker-entrypoint.sh
@@ -1,3 +1,42 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+function addProperty() {
+  local path=$1
+  local name=$2
+  local value=$3
+
+  local entry="<property><name>$name</name><value>${value}</value></property>"
+  local escapedEntry=$(echo $entry | sed 's/\//\\\//g')
+  echo "escapedEntry = $escapedEntry"
+  sed -i "/<\/configuration>/i\\${escapedEntry}\n" $path
+}
+
+function configure() {
+    local path=$1
+    local module=$2
+    local envPrefix=$3
+
+    local var
+    local value
+
+    echo "Configuring $module"
+    for c in `printenv | perl -sne 'print "$1 " if m/^${envPrefix}_(.+?)=.*/' -- -envPrefix=$envPrefix`; do
+        name=`echo ${c} | perl -pe 's/___/-/g; s/__/_/g; s/_/./g'`
+        var="${envPrefix}_${c}"
+        value=${!var}
+        echo " - Setting $name=$value"
+        addProperty $path $name "$value"
+    done
+}
+
+configure /etc/hadoop/core-site.xml core CORE_CONF
+configure /etc/hadoop/hdfs-site.xml hdfs HDFS_CONF
+configure /etc/hadoop/yarn-site.xml yarn YARN_CONF
+configure /etc/hadoop/httpfs-site.xml httpfs HTTPFS_CONF
+configure /etc/hadoop/kms-site.xml kms KMS_CONF
+configure /etc/hadoop/mapred-site.xml mapred MAPRED_CONF
+configure ${SPARK_HOME}/conf/hive-site.xml hive HIVE_SITE_CONF
+
+
 dockerize -wait tcp://${DB_PG_IP}:${DB_PG_PORT} -wait-retry-interval 1s -timeout 60s
 java ${JVM_OPTS} -jar /server/target/kun-infra.jar
