ef5250421a33868a2e3eb782faecdb026a3e507c
aijia <aijia.guo@miotech.com>
2021-10-28 17:36:53 +0800

    operator multi env

--- kun-commons/kun-commons-utils/src/main/java/com/miotech/kun/commons/utils/ExprUtils.java
+++ kun-commons/kun-commons-utils/src/main/java/com/miotech/kun/commons/utils/ExprUtils.java
@@ -1,8 +1,73 @@
 package com.miotech.kun.commons.utils;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import freemarker.template.*;
+import org.slf4j.LoggerFactory;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+
 public class ExprUtils {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExprUtils.class);
+
     public static String evalExecuteTimeExpr(String expr, String tick) {
-        return expr.replace("${execute_time}", tick);
+        return evalExpr(expr, ImmutableMap.of("execute_time", tick));
+    }
+
+    public static String evalExpr(String expr, Map<String, Object> dataModel) {
+        try {
+            Template template = new Template("parseTemplate", expr, new Configuration(new Version("2.3.30")));
+            StringWriter result = new StringWriter();
+            template.process(addBuiltinMethods(dataModel), result);
+            logger.info("expr resolved to: " + result);
+            return result.toString();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static Map<String, Object> addBuiltinMethods(Map<String, Object> dataModel) {
+        return ImmutableMap.<String, Object>builder()
+                .putAll(dataModel)
+                .put("ref", new RefMethod(dataModel))
+                .build();
+    }
+
+    private static class RefMethod implements TemplateMethodModelEx {
+        private Map<String, Object> dataModel;
+
+        public RefMethod(Map<String, Object> dataModel) {
+            this.dataModel = dataModel;
+        }
+
+        @Override
+        public String exec(List arguments) throws TemplateModelException {
+            String targetName = (String) dataModel.get("target.schema");
+
+            // length = 1
+            String arg1 = ((SimpleScalar) arguments.get(0)).getAsString();
+            String[] dbAndTable = arg1.split("\\.");
+            String db = dbAndTable[0];
+            String resolvedDb = "prod".equals(targetName) || Strings.isNullOrEmpty(targetName) ? db : db + "_" + targetName;
+            String table = dbAndTable[1];
+            String resolvedDbTable = resolvedDb + "." + table;
+
+            // length = 2
+            List<String> vars = new ArrayList<>();
+            vars.add(resolvedDbTable);
+            vars.addAll((List<String>) arguments.subList(1, arguments.size())
+                    .stream()
+                    .map(x ->  dataModel.get(((SimpleScalar)x).getAsString()))
+                    .collect(Collectors.toList()));
+
+            return String.join("_", vars);
+        }
     }
 }
--- kun-data-platform/kun-workflow-operators/spark-operators/src/test/java/com/miotech/kun/workflow/operator/SparkSqlOperatorV2Test.java
+++ kun-data-platform/kun-workflow-operators/spark-operators/src/test/java/com/miotech/kun/workflow/operator/SparkSqlOperatorV2Test.java
@@ -43,7 +43,7 @@ public class SparkSqlOperatorV2Test {
         context.setParam(SPARK_CONF, "");
         context.setParam(SPARK_PROXY_USER, "hadoop");
         context.setParam(SPARK_APPLICATION, "s3://bucket/sql.jar");
-        context.setParam(SPARK_APPLICATION_ARGS, " select * from db.table_${execute_time} ");
+        context.setParam(SPARK_APPLICATION_ARGS, " select * from ${ref('db.table')}_${execute_time} ");
         context.setParam(SPARK_YARN_HOST, "http://localhost:8088");
         context.setParam(CONF_LINEAGE_OUTPUT_PATH, "");
         context.setParam(CONF_LINEAGE_JAR_PATH, "");
@@ -117,7 +117,7 @@ public class SparkSqlOperatorV2Test {
         {
             e.printStackTrace();
         }
-        assertTrue(contentBuilder.toString().trim().equals("select * from db.table_000000000000"));
+        assertTrue(contentBuilder.toString().trim().equals("select * from db_test.table_000000000000"));
         assertTrue (cmd.get(len-2).endsWith("-f"));
         assertTrue (cmd.get(len-3).endsWith("s3://bucket/sql.jar"));
     }
