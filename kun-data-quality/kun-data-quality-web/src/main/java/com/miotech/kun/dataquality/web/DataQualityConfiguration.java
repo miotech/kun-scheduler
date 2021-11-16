e16c02a346cd6e0cec92c6a967a1d3a90f76d378
seaway-z <xinwei.zhang@miotech.com>
2021-11-16 11:40:45 +0800

    Add kun-data-quality-web module

---
+++ kun-data-quality/kun-data-quality-web/src/main/java/com/miotech/kun/dataquality/web/DataQualityConfiguration.java
@@ -0,0 +1,15 @@
+package com.miotech.kun.dataquality.web;
+
+/**
+ * @author: Jie Chen
+ * @created: 2020/7/17
+ */
+public class DataQualityConfiguration {
+
+    public static final String METADATA_DATASOURCE_URL = "metadata.datasource.url";
+    public static final String METADATA_DATASOURCE_USERNAME = "metadata.datasource.username";
+    public static final String METADATA_DATASOURCE_PASSWORD = "metadata.datasource.password";
+    public static final String METADATA_DATASOURCE_DIRVER_CLASS = "metadata.datasource.driver-class-name";
+
+    public static final String WORKFLOW_OPERATOR_NAME = "data-quality-operator";
+}
---
+++ kun-data-quality/kun-data-quality-web/src/main/java/com/miotech/kun/dataquality/web/model/bo/DataQualityRequest.java
@@ -0,0 +1,43 @@
+package com.miotech.kun.dataquality.web.model.bo;
+
+import com.miotech.kun.dataquality.web.model.entity.DataQualityRule;
+import lombok.Data;
+import org.json.simple.JSONObject;
+
+import java.util.List;
+
+/**
+ * @author: Jie Chen
+ * @created: 2020/7/16
+ */
+@Data
+public class DataQualityRequest {
+
+    private String name;
+
+    private List<String> types;
+
+    private String description;
+
+    private String dimension;
+
+    private JSONObject dimensionConfig;
+
+    private List<DataQualityRule> validateRules;
+
+    private List<Long> relatedTableIds;
+
+    private Long primaryDatasetGid;
+
+    private Long taskId;
+
+    private String createUser;
+
+    private Long createTime;
+
+    private String updateUser;
+
+    private Long updateTime;
+
+    private Boolean isBlocking;
+}
