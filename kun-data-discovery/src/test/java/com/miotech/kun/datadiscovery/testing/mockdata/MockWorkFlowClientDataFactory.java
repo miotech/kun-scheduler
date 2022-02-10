781d163e3a9a54678fe6b5e99fc42e9f6f3a6a17
zemin huang <96981958+via-red@users.noreply.github.com>
2022-02-10 09:46:24 +0800

    update  LineageAppServiceTest (#791)
    * update  LineageAppServiceTest
    
    * update  LineageAppServiceTest

--- kun-data-discovery/src/test/java/com/miotech/kun/datadiscovery/testing/mockdata/MockWorkFlowClientDataFactory.java
+++ kun-data-discovery/src/test/java/com/miotech/kun/datadiscovery/testing/mockdata/MockWorkFlowClientDataFactory.java
@@ -8,13 +8,17 @@ import com.miotech.kun.workflow.core.model.lineage.EdgeInfo;
 import com.miotech.kun.workflow.core.model.lineage.EdgeTaskInfo;
 import com.miotech.kun.workflow.core.model.task.Task;
 import com.miotech.kun.workflow.core.model.taskrun.TaskRunStatus;
+import com.miotech.kun.workflow.utils.DateTimeUtils;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.apache.commons.compress.utils.Lists;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @program: kun
@@ -24,19 +28,12 @@ import java.util.Map;
  **/
 public class MockWorkFlowClientDataFactory {
 
+    public static Task getTask(long id, String testDownTask) {
+        return Task.newBuilder().withId(id).withName(testDownTask)
+                .withDependencies(new ArrayList<>()).build();
 
-    public static EdgeInfo createEdgeInfo() {
-        ArrayList<EdgeTaskInfo> taskInfos = Lists.newArrayList();
-        EdgeTaskInfo taskInfo_1 = EdgeTaskInfo.newBuilder()
-                .withId(1L)
-                .withName("taskInfoOne")
-                .build();
-        taskInfos.add(taskInfo_1);
-        EdgeTaskInfo taskInfo_2 = EdgeTaskInfo.newBuilder()
-                .withId(2L)
-                .withName("taskInfoTwo")
-                .build();
-        taskInfos.add(taskInfo_2);
+    }
+    public static EdgeInfo createEdgeInfo(ArrayList<EdgeTaskInfo> taskInfos) {
         EdgeInfo edgeInfo = EdgeInfo.newBuilder()
                 .withTaskInfos(taskInfos)
                 .withUpstreamDatasetGid(1L)
@@ -45,50 +42,54 @@ public class MockWorkFlowClientDataFactory {
         return edgeInfo;
     }
 
-    public static Map<Long, List<TaskRun>>  createLatestTaskRuns() {
-        Map<Long, List<TaskRun>> map=new HashMap<>();
-        ArrayList<TaskRun> taskRunArrayList = Lists.newArrayList();
-        TaskRun taskRun1 = TaskRun.newBuilder().withId(1L).withCreatedAt(OffsetDateTime.now()).withStatus(TaskRunStatus.SUCCESS).build();
-        TaskRun taskRun2 = TaskRun.newBuilder().withId(2L).withStatus(TaskRunStatus.FAILED).build();
-        TaskRun taskRun3 = TaskRun.newBuilder().withId(3L).withStatus(TaskRunStatus.SUCCESS).build();
-        taskRunArrayList.add(taskRun1);
-        taskRunArrayList.add(taskRun2);
-        taskRunArrayList.add(taskRun3);
-        map.put(1L,taskRunArrayList);
-        map.put(2L,taskRunArrayList);
 
+    public static EdgeTaskInfo getEdgeTaskInfo(Long taskId, String taskName) {
+        EdgeTaskInfo taskInfo_1 = EdgeTaskInfo.newBuilder()
+                .withId(taskId)
+                .withName(taskName)
+                .build();
+        return taskInfo_1;
+    }
+
+    public   static TaskRun getTaskRun(long id, String dateTimeString, TaskRunStatus success) {
+        TaskRun taskRun = TaskRun.newBuilder().withId(id).withStartAt(DateTimeUtils.freezeAt(dateTimeString))
+                .withStatus(success).build();
+        return taskRun;
+    }
+    public static Map<Long, List<TaskRun>>  createLatestTaskRuns(Long taskId,List<TaskRun> taskRunList) {
+        Map<Long, List<TaskRun>> map=new HashMap<>();
+        List<TaskRun> taskRuns = taskRunList.stream().sorted((r1, r2) -> r2.getId().compareTo(r1.getId())).collect(Collectors.toList());
+        map.put(taskId,taskRuns);
         return map;
     }
 
-    public static DatasetLineageInfo createLineageNeighborsGraph() {
-        List<DatasetNodeInfo> up=new ArrayList<>();
+    public static DatasetLineageInfo createLineageNeighborsGraph(DatasetNodeInfo nodeInfo ,List<DatasetNodeInfo> up,List<DatasetNodeInfo> down) {
 
-        List<DatasetNodeInfo> down=new ArrayList<>();
-        DatasetNodeInfo nodeInfo = DatasetNodeInfo
-                .newBuilder()
-                .withGid(1L)
-                .withDatasetName("testDataSetNameOne")
-                .withDownstreamDatasetCount(1)
-                .withDownstreamDatasetCount(1)
-                .build();
-        up.add(nodeInfo);
-        down.add(nodeInfo);
         DatasetLineageInfo lineageInfo = DatasetLineageInfo.newBuilder()
                 .withSourceNode(nodeInfo)
-                .withUpstreamNodes(up).withDownstreamNodes(down).build();
+                .withUpstreamNodes(up)
+                .withDownstreamNodes(down)
+                .build();
         return lineageInfo;
     }
-    public static DatasetLineageInfo createLineageNeighbors() {
-        ArrayList<Task> downTask = Lists.newArrayList();
-        Task testDownTask = Task.newBuilder().withId(1L).withName("testDownTask").withDependencies(Lists.newArrayList()).build();
-        downTask.add(testDownTask);
-        ArrayList<Task> upTask = Lists.newArrayList();
-        Task testUpTask = Task.newBuilder().withId(2L).withName("testUpTask").withDependencies(Lists.newArrayList()).build();
-        upTask.add(testUpTask);
+
+
+    public static DatasetNodeInfo getDatasetNodeInfo(Long  gid, String dataSetName) {
+        DatasetNodeInfo upNodeInfo = DatasetNodeInfo
+                .newBuilder()
+                .withGid(gid)
+                .withDatasetName(dataSetName)
+                .withDownstreamDatasetCount(1)
+                .withUpstreamDatasetCount(1)
+                .build();
+        return upNodeInfo;
+    }
+
+    public static DatasetLineageInfo createLineageNeighbors(ArrayList<Task> upTask, ArrayList<Task> downTask,Long dataSetId) {
         DatasetNodeInfo nodeInfo = DatasetNodeInfo
                 .newBuilder()
-                .withGid(1L)
-                .withDatasetName("testDataSetNameOne")
+                .withGid(dataSetId)
+                .withDatasetName("taskInfoOne")
                 .withUpstreamTasks(upTask)
                 .withDownstreamTasks(downTask)
                 .build();
@@ -98,13 +99,16 @@ public class MockWorkFlowClientDataFactory {
         return lineageInfo;
     }
 
-    public static List<LineageDatasetBasic> createLineageDatasetBasicList() {
-        List<LineageDatasetBasic>  lineageDatasetBasicList=new ArrayList<>();
-        LineageDatasetBasic lineageDatasetBasic=new LineageDatasetBasic();
-        lineageDatasetBasic.setGid(1L);
-        lineageDatasetBasic.setName("testLineageDatasetBasic");
-        lineageDatasetBasicList.add(lineageDatasetBasic);
+    public static List<LineageDatasetBasic> createLineageDatasetBasicList(List<DatasetNodeInfo> datasetNodeInfoList) {
+        List<LineageDatasetBasic> lineageDatasetBasicList = datasetNodeInfoList.stream().map(datasetNodeInfo -> {
+            LineageDatasetBasic lineageDatasetBasic = new LineageDatasetBasic();
+            lineageDatasetBasic.setGid(datasetNodeInfo.getGid());
+            lineageDatasetBasic.setName(datasetNodeInfo.getDatasetName());
+            return lineageDatasetBasic;
+        }).collect(Collectors.toList());
         return lineageDatasetBasicList;
 
     }
+
+
 }
