bb3a554e97902dd2cbbb1ee3cfbc813a0b11655e
seaway-z <xinwei.zhang@miotech.com>
2022-07-01 09:48:15 +0800

    fix expectation_run sort

--- kun-data-quality/kun-data-quality-web/src/main/java/com/miotech/kun/dataquality/web/common/service/ExpectationRunService.java
+++ kun-data-quality/kun-data-quality-web/src/main/java/com/miotech/kun/dataquality/web/common/service/ExpectationRunService.java
@@ -1,14 +1,9 @@
 package com.miotech.kun.dataquality.web.common.service;
 
-import com.google.common.base.Preconditions;
-import com.miotech.kun.dataquality.core.expectation.ValidationResult;
 import com.miotech.kun.dataquality.facade.ExpectationRunFacade;
 import com.miotech.kun.dataquality.web.common.dao.ExpectationRunDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.OffsetDateTime;
-import java.util.List;
 
 @Service
 public class ExpectationRunService implements ExpectationRunFacade {
@@ -16,9 +11,4 @@ public class ExpectationRunService implements ExpectationRunFacade {
     @Autowired
     private ExpectationRunDao expectationRunDao;
 
-    @Override
-    public List<ValidationResult> fetchByUpdateTimeFromAndPassed(OffsetDateTime updateTimeFrom, boolean passed) {
-        Preconditions.checkNotNull(updateTimeFrom, "Argument `updateTimeFrom` should not be null.");
-        return expectationRunDao.fetchByUpdateTimeFromAndPassed(updateTimeFrom, passed);
-    }
 }
