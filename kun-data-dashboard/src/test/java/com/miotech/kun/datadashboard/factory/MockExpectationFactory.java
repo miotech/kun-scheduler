1f665f3afe7ef29c31f4fadbaf145ad2d81acd3d
seaway-z <xinwei.zhang@miotech.com>
2022-07-20 11:58:20 +0800

    Fix bugs with a test pass rate of more than 100%

---
+++ kun-data-dashboard/src/test/java/com/miotech/kun/datadashboard/factory/MockExpectationFactory.java
@@ -0,0 +1,59 @@
+package com.miotech.kun.datadashboard.factory;
+
+import com.google.common.collect.ImmutableList;
+import com.miotech.kun.commons.utils.DateTimeUtils;
+import com.miotech.kun.commons.utils.IdGenerator;
+import com.miotech.kun.dataquality.core.assertion.EqualsAssertion;
+import com.miotech.kun.dataquality.core.expectation.*;
+import com.miotech.kun.dataquality.core.metrics.Metrics;
+import com.miotech.kun.dataquality.core.metrics.SQLMetrics;
+
+public class MockExpectationFactory {
+
+    private MockExpectationFactory() {
+    }
+
+    public static Expectation create(Long datasetGid) {
+        Dataset dataset = Dataset.builder().gid(datasetGid).dataSource(null).build();
+        return Expectation.newBuilder()
+                .withExpectationId(IdGenerator.getInstance().nextId())
+                .withName("Expectation Name")
+                .withDescription("Expectation Desc")
+                .withMethod(new JDBCExpectationMethod("select count(1) c from demo",
+                        ImmutableList.of(new JDBCExpectationAssertion("c", JDBCExpectationAssertion.ComparisonOperator.EQUALS, "=", "NUMBER", "0"))))
+                .withMetrics(new SQLMetrics("sql metrics", "desc", Metrics.Granularity.CUSTOM, dataset, "select count(1) c from demo", "c"))
+                .withAssertion(new EqualsAssertion("=", "0"))
+                .withTrigger(Expectation.ExpectationTrigger.SCHEDULED)
+                .withDataset(dataset)
+                .withTaskId(IdGenerator.getInstance().nextId())
+                .withCaseType(CaseType.SKIP)
+                .withCreateTime(DateTimeUtils.now())
+                .withUpdateTime(DateTimeUtils.now())
+                .withCreateUser("admin")
+                .withUpdateUser("admin")
+                .build();
+    }
+
+    public static Expectation createWithTaskId(Long taskId) {
+        return Expectation.newBuilder()
+                .withExpectationId(IdGenerator.getInstance().nextId())
+                .withName("Expectation Name")
+                .withDescription("Expectation Desc")
+                .withMethod(new JDBCExpectationMethod("select count(1) c from demo",
+                        ImmutableList.of(new JDBCExpectationAssertion("c", JDBCExpectationAssertion.ComparisonOperator.EQUALS, "=", "NUMBER", "0"))))
+                .withTrigger(Expectation.ExpectationTrigger.SCHEDULED)
+                .withDataset(Dataset.builder().gid(IdGenerator.getInstance().nextId()).dataSource(null).build())
+                .withTaskId(taskId)
+                .withCaseType(CaseType.BLOCK)
+                .withCreateTime(DateTimeUtils.now())
+                .withUpdateTime(DateTimeUtils.now())
+                .withCreateUser("admin")
+                .withUpdateUser("admin")
+                .build();
+    }
+
+    public static Expectation create() {
+        return create(IdGenerator.getInstance().nextId());
+    }
+
+}
--- kun-data-dashboard/src/test/java/com/miotech/kun/datadashboard/persistence/DataQualityRepositoryTest.java
+++ kun-data-dashboard/src/test/java/com/miotech/kun/datadashboard/persistence/DataQualityRepositoryTest.java
@@ -1,15 +1,34 @@
 package com.miotech.kun.datadashboard.persistence;
 
+import com.google.common.collect.ImmutableList;
+import com.miotech.kun.commons.db.sql.DefaultSQLBuilder;
+import com.miotech.kun.commons.utils.IdGenerator;
 import com.miotech.kun.datadashboard.DataDashboardTestBase;
+import com.miotech.kun.datadashboard.factory.MockExpectationFactory;
+import com.miotech.kun.datadashboard.factory.MockValidationResultFactory;
 import com.miotech.kun.datadashboard.model.bo.TestCasesRequest;
 import com.miotech.kun.datadashboard.model.entity.AbnormalDatasets;
+import com.miotech.kun.dataquality.core.expectation.Expectation;
+import com.miotech.kun.dataquality.core.expectation.ValidationResult;
+import com.miotech.kun.workflow.utils.JSONUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 
 public class DataQualityRepositoryTest extends DataDashboardTestBase {
+
+    private static final String EXPECTATION_TABLE_NAME = "kun_dq_expectation";
+    private static final String EXPECTATION_RUN_TABLE_NAME = "kun_dq_expectation_run";
+    private static final List<String> EXPECTATION_INSERT_COLUMNS = ImmutableList.of("id", "name", "types", "description", "method", "metrics_config", "assertion_config", "trigger",
+            "dataset_gid", "task_id", "case_type", "create_time", "update_time", "create_user", "update_user");
+    private static final List<String> EXPECTATION_RUN_INSERT_COLUMNS = ImmutableList.of("expectation_id", "passed", "execution_result", "assertion_result", "continuous_failing_count", "update_time");
+
 
     @Autowired
     private DataQualityRepository dataQualityRepository;
@@ -20,4 +39,104 @@ public class DataQualityRepositoryTest extends DataDashboardTestBase {
         AbnormalDatasets abnormalDatasets = dataQualityRepository.getAbnormalDatasets(request);
         assertThat(abnormalDatasets.getAbnormalDatasets(), empty());
     }
+
+    @Test
+    public void testGetSuccessCount_expectationAlreadyDeleted() {
+        Long expectationId = IdGenerator.getInstance().nextId();
+        ValidationResult validationResult = MockValidationResultFactory.create(expectationId, true);
+        insertExpectationRun(validationResult);
+
+        Long totalCaseCount = dataQualityRepository.getTotalCaseCount();
+        Long successCount = dataQualityRepository.getSuccessCount();
+        Long expectationRunTotalCount = getExpectationRunTotalCount();
+        assertThat(totalCaseCount, is(0L));
+        assertThat(successCount, is(0L));
+        assertThat(expectationRunTotalCount, is(1L));
+    }
+
+    @Test
+    public void testGetSuccessCount_sameUpdateTime() {
+        Expectation expectation1 = MockExpectationFactory.create();
+        Expectation expectation2 = expectation1.cloneBuilder().withExpectationId(IdGenerator.getInstance().nextId()).withName("expectation2").withUpdateTime(expectation1.getUpdateTime()).build();
+
+        insertExpectation(expectation1);
+        insertExpectation(expectation2);
+
+        Long expectationTotalCount = getExpectationTotalCount();
+        assertThat(expectationTotalCount, is(2L));
+
+        Long expectationId = expectation1.getExpectationId();
+        ValidationResult validationResult = MockValidationResultFactory.create(expectationId, true).cloneBuilder().withUpdateTime(expectation1.getUpdateTime()).build();
+        insertExpectationRun(validationResult);
+
+        Long successCount = dataQualityRepository.getSuccessCount();
+        assertThat(successCount, is(1L));
+    }
+
+    @Test
+    public void testGetLongExistingCount() {
+        long continuousFailingCountBelowThreshold = 1;
+        long continuousFailingCountAboveThreshold = 100;
+
+        Expectation expectation = MockExpectationFactory.create();
+        insertExpectation(expectation);
+        ValidationResult validationResult1 = MockValidationResultFactory.create(expectation.getExpectationId(), false).cloneBuilder().withContinuousFailingCount(continuousFailingCountBelowThreshold).build();
+        ValidationResult validationResult2 = MockValidationResultFactory.create(expectation.getExpectationId(), false).cloneBuilder().withContinuousFailingCount(continuousFailingCountAboveThreshold).build();
+        insertExpectationRun(validationResult1);
+        insertExpectationRun(validationResult2);
+
+        Long longExistingCount = dataQualityRepository.getLongExistingCount();
+        assertThat(longExistingCount, is(1L));
+    }
+
+    private Long getExpectationTotalCount() {
+        String sql = "select count(1) c from kun_dq_expectation";
+        return jdbcTemplate.queryForObject(sql, Long.class);
+    }
+
+    private Long getExpectationRunTotalCount() {
+        String sql = "select count(1) c from kun_dq_expectation_run";
+        return jdbcTemplate.queryForObject(sql, Long.class);
+    }
+
+    private void insertExpectation(Expectation expectation) {
+        String sql = DefaultSQLBuilder.newBuilder()
+                .insert(EXPECTATION_INSERT_COLUMNS.toArray(new String[0]))
+                .into(EXPECTATION_TABLE_NAME)
+                .asPrepared()
+                .getSQL();
+        jdbcTemplate.update(sql,
+                expectation.getExpectationId(),
+                expectation.getName(),
+                StringUtils.join(expectation.getTypes(), ","),
+                expectation.getDescription(),
+                JSONUtils.toJsonString(expectation.getMethod()),
+                JSONUtils.toJsonString(expectation.getMetrics()),
+                JSONUtils.toJsonString(expectation.getAssertion()),
+                expectation.getTrigger().name(),
+                expectation.getDataset().getGid(),
+                expectation.getTaskId(),
+                expectation.getCaseType().name(),
+                expectation.getCreateTime(),
+                expectation.getUpdateTime(),
+                expectation.getCreateUser(),
+                expectation.getUpdateUser());
+    }
+
+    private void insertExpectationRun(ValidationResult validationResult) {
+        String sql = DefaultSQLBuilder.newBuilder()
+                .insert(EXPECTATION_RUN_INSERT_COLUMNS.toArray(new String[0]))
+                .into(EXPECTATION_RUN_TABLE_NAME)
+                .asPrepared()
+                .getSQL();
+
+        jdbcTemplate.update(sql,
+                validationResult.getExpectationId(),
+                validationResult.isPassed(),
+                validationResult.getExecutionResult(),
+                JSONUtils.toJsonString(validationResult.getAssertionResults()),
+                validationResult.getContinuousFailingCount(),
+                validationResult.getUpdateTime());
+    }
+
 }
